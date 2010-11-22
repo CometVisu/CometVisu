84dcaa740e4618e287c25267c55745dc8761b351
mayerch <mayerch@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2010-11-22 20:12:20 +0000

    Release CometVisu 0.5.1

---
+++ CometVisu/branches/release_0.5.1/visu/designs/structure_custom.js
@@ -0,0 +1,75 @@
+/* visudesign_custom.js (c) 2010 by Christian Mayer [CometVisu at ChristianMayer dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+*/
+
+/**
+ * This class defines all the custom changes to the visu
+ */
+function VisuDesign_Custom() {};                // do NOT change here
+VisuDesign_Custom.prototype = new VisuDesign(); // do NOT chagen here
+
+/**
+ * Custom changes could go here and look e.g. like
+****************************************
+VisuDesign_Custom.prototype.addCreator("line", {
+      create: function( page, path ) {
+              return $( '<hr />' );
+          },
+      attributes: {
+      },
+      content: false
+});
+****************************************
+ */
+
+/**
+ * This is a custom function that extends the available widgets.
+ * It's purpose is to change the design of the visu during runtime
+ * to demonstrate all available
+ */
+VisuDesign_Custom.prototype.addCreator("designtoggle", {
+      create: function( page, path ) {
+                var ret_val = $('<div class="widget" />');
+                ret_val.addClass( 'switch' );
+                var label = '<div class="label">' + page.textContent + '</div>';
+                var actor = '<div class="actor switchUnpressed">';
+                var value = $('link[href*="designs"]').attr('href').split('/')[1];
+                actor += '<div class="value">' + value + '</div>';
+                actor += '</div>';
+                ret_val.append(label).append($(actor)
+                    .data({
+                        'mapping' : $(page).attr('mapping'),
+                        'styling' : $(page).attr('styling'),
+                        'value'   : value,
+                        'type'    : 'toggle'
+                    })
+                    .bind('click', function() {
+                            var designs     = [ 'pure', 'discreet' ];
+                            var oldDesign = $('.value',this).text();
+                            var newDesign = designs[ (designs.indexOf(oldDesign) + 1) % designs.length ];
+                            $('.value',this).text(newDesign);
+                            $('link[href*="designs"]').each(function(){
+                                this.href = this.href.replace( oldDesign, newDesign );
+                            });
+                        })
+                );
+                return ret_val;
+          },
+      attributes: {
+      },
+      content: {type: "string", required: true}
+});
+
---
+++ CometVisu/branches/release_0.5.1/visu/edit/save_config.php
@@ -0,0 +1,139 @@
+<?php
+define("CONFIG_FILENAME", "../visu_config%s.xml");
+define("CONFIG_BASE_XML", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><pages />");
+define("CONFIG_PAGE_XML", "<page />");
+
+// Übergabe-Parameter annehmen
+$strJson   = (true === isset($_POST['data']))   ? $_POST['data']   : null;
+$strConfig = (true === isset($_POST['config'])) ? $_POST['config'] : null;
+
+// Dateinamen reinigen!
+$strConfig = preg_replace("/[^\-\_0-9da-z]/i", "", $strConfig);
+
+// Dateinamen der Config festlegen
+$strConfig = sprintf(CONFIG_FILENAME, $strConfig);
+// auf fully qualified filename wechseln
+$strConfig = realpath($strConfig);
+
+if (false === is_writeable($strConfig)) {
+    echo json_encode("config-file is not writeable by webserver-process; please chmod/chown config-file '$strConfig'.");
+    exit;
+}
+
+// wenn wir kein JSON bekamen, bzw das leer ist, dann speichern wir das auch nicht
+if (true === empty($strJson)) {
+    echo 0;
+    exit;
+}
+// JSON dekodieren und PHP-geeignetes Array-Object-Kram daraus erzeugen
+$objConfig = json_decode(stripslashes($strJson));
+
+/** die alten Mappings und stylings übernehmen */
+$objDOM = new DOMDocument("1.0", "UTF-8");
+$objDOM->load($strConfig);
+$objDOM->formatOutput = true;
+
+
+$objPages = $objDOM->getElementsByTagName("pages")->item(0);
+
+$objTmp = $objPages->getElementsByTagName("page");
+foreach ($objTmp as $objTmpNode) {
+    $objPages->removeChild($objTmpNode);
+}
+ /** mappings und stylings übernommen */
+
+$objPages->appendChild(createDOMFromJSON($objConfig));
+$objDOM->appendChild($objPages);
+
+
+// doofer Hack - das XML zweimal erzeugen damit
+// Newlines und Einrueckungen passen
+$strXML = $objDOM->saveXML();
+$objXML = new DOMDocument();
+$objXML->preserveWhiteSpace = false;
+$objXML->formatOutput = true;
+$objXML->loadXML($strXML);
+
+// XML in die Konfigurationsdatei speichern
+$handle = fopen($strConfig, "w");
+fputs($handle, $objXML->saveXML());
+fclose($handle);
+
+echo 1;
+exit;
+
+
+/**
+ * Erzeugt zu einem JSON-Objekt (= der Konfiguration)
+ * den DOM-Baum mit allen Ästen und Blättern
+ *
+ * @param   object  $obj    Ein Objekt das aus dem JSON gewonnen wurde
+ * @returns object          DOM-Objekt
+ * @throws  Exception       Wenn die Konfiguration malformed ist
+ */
+function createDOMFromJSON($objJSON) {
+
+    global $objDOM;
+
+    if (false === isset($objJSON->_type) || "page" != $objJSON->_type) {
+        throw new Exception("Malformed config received!");
+    }
+
+    $objXML = $objDOM->createElement("page");
+
+    if (false === empty($objJSON->name)) {
+        // den Namen der Seite festlegen
+        $objXML->setAttribute("name", (string)$objJSON->name);
+    }
+
+    if (true === empty($objJSON->_elements) || false === is_array($objJSON->_elements)) {
+        return $objXML;
+    }
+
+    foreach ($objJSON->_elements as $objElement) {
+        if ("page" == $objElement->_type) {
+            $objXML->appendChild(createDOMFromJSON($objElement));
+        } else {
+            $objXML->appendChild(createChildFromJSON($objElement));
+        }
+    }
+
+    return $objXML;
+}
+
+/**
+ * Erzeugt aus einem winzigen Teil des JSON-Objekts (= der Konfiguration)
+ * ein einzelnes Blatt
+ *
+ * @param   object  $obj    Ein Objekt das aus dem JSON gewonnen wurde
+ * @returns object          DOM-Objekt
+ */
+function createChildFromJSON($objJSON) {
+    global $objDOM;
+
+    if (true === empty($objJSON->_type)) {
+        return null;
+    }
+
+    $objXML = $objDOM->createElement($objJSON->_type);
+    if (false === empty($objJSON->_text)) {
+        $objXML->nodeValue = $objJSON->_text;
+    }
+
+    foreach ($objJSON as $strAttribute => $strValue) {
+        if (0 === strpos($strAttribute, "_")) {
+            // Parameter die mit "_" beginnen sind special purpose
+            continue;
+        }
+
+        if ($strAttribute === "textContent") {
+            $objXML->nodeValue = $strValue;
+            continue;
+        }
+        $objXML->setAttribute($strAttribute, $strValue);
+    }
+
+    return $objXML;
+}
+
+?>
