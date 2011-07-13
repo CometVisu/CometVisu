f0a8b381e8cf92db0ec3fcdd3e74cce7d620e534
makki1 <Makki1@users.noreply.github.com>
2011-07-13 18:15:11 +0000

    Add gweather (Google)-Plugin, withdraw zweather, Yahoo data is crap

---
+++ visu/plugins/gweather/gweather.php
@@ -0,0 +1,8 @@
+<?php
+header("content-type: text/xml");
+$city = $_GET['city'];
+$lang = $_GET['lang'];
+
+$xmlData = utf8_encode(file_get_contents("http://www.google.com/ig/api?hl=$lang&weather=$city"));
+echo $xmlData;
+?>
