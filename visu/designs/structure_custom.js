fac0de9f43383213677e3af9607b5cb2e290a9b0
Christian Mayer <ChristianMayer@users.noreply.github.com>
2012-04-02 21:49:52 +0000

    Preparation for cleaner file structure and automated build script: * initial support for automatic documentation generation (using YUIdoc)

--- visu/designs/structure_custom.js
+++ visu/designs/structure_custom.js
@@ -13,15 +13,21 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/**
+ * This module defines the custom widget additions to the CometVisu visualisation.
+ * @module Structure custom
 */
 
 /**
  * This class defines all the custom changes to the visu
+ * @class VisuDesign_Custom
  */
 function VisuDesign_Custom() {};                // do NOT change here
 VisuDesign_Custom.prototype = new VisuDesign(); // do NOT change here
 
-/**
+/*
  * Custom changes could go here and look e.g. like
 ****************************************
 VisuDesign_Custom.prototype.addCreator("line", {
@@ -35,7 +41,7 @@ VisuDesign_Custom.prototype.addCreator("line", {
 ****************************************
  */
 
-/**
+/*
  * This is a custom function that extends the available widgets.
  * It's purpose is to change the design of the visu during runtime
  * to demonstrate all available
