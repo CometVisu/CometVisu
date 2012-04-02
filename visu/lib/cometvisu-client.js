cac0e15b253a33798913aface1478dfb7ff1d2f6
Christian Mayer <ChristianMayer@users.noreply.github.com>
2012-04-02 17:42:08 +0000

    Preparation for cleaner file structure and automated build script: * separation of CometVisu libraries and external dependencies

--- visu/lib/cometvisu-client.js
+++ visu/lib/cometvisu-client.js
@@ -13,10 +13,15 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *
+ * @module CometVisu Client
+ * @title  CometVisu Client
 */
 
 /**
  * Class that handles the communicaton of the client
+ * @class CometVisu
+ * @param {String} urlPrefix The address of the service
  */
 function CometVisu( urlPrefix )
 {
