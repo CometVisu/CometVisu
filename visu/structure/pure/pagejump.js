b46627a988fe4674aae82a35b45321d03d085523
Christian Mayer <ChristianMayer@users.noreply.github.com>
2012-05-06 11:12:01 +0000

    New feature: initial implementation of <pagejump> widget

---
+++ visu/structure/pure/pagejump.js
@@ -0,0 +1,68 @@
+/* trigger.js (c) 2012 by Christian Mayer [CometVisu at ChristianMayer dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+basicdesign.addCreator('pagejump', {
+  create: function( element, path ) {
+    var $e = $(element);
+    var layout = $e.children('layout')[0];
+    var style = layout ? 'style="' + extractLayout( layout ) + '"' : '';
+    var target = $e.attr('target') ? $e.attr('target') : '0';
+    var ret_val = $('<div class="widget clearfix switch" ' + style + ' />');
+    ret_val.setWidgetLayout($e);
+    var labelElement = $e.find('label')[0];
+    var label = labelElement ? '<div class="label">' + labelElement.textContent + '</div>' : '';
+    var address = makeAddressList($e);
+    var actor = '<div class="actor switchUnpressed ';
+    if ( $e.attr( 'align' ) ) 
+      actor += $e.attr( 'align' ); 
+    actor += '">';
+    if( $e.attr( 'name' ) )
+      actor += '<div class="value">' + $e.attr( 'name' ) + '</div>';
+    else
+      actor += '<div class="value">' + target + '</div>';
+    actor += '</div>';
+    var $actor = $(actor).data( {
+      'styling' : $(element).attr('styling'),
+      'type'    : 'pagejump',
+      'align'   : $e.attr('align'),
+      'target'  : target
+    } ).bind( 'click', this.action ).bind( 'mousedown', function(){
+      $(this).removeClass('switchUnpressed').addClass('switchPressed');
+    } ).bind( 'mouseup mouseout', function(){ // not perfect but simple
+      $(this).removeClass('switchPressed').addClass('switchUnpressed');
+    } ).setWidgetStyling(target);
+    ret_val.append( label ).append( $actor );
+    return ret_val;
+  },
+  action: function() {
+    var data = $(this).data();
+    scrollToPage( data.target );
+  },
+  attributes: {
+    traget:  { type: 'string' , required: true  },
+    name:    { type: 'string' , required: false },
+    styling: { type: 'styling', required: false },
+    align:   { type: 'string' , required: false },
+    colspan: { type: 'numeric', required: false },
+    rowspan: { type: 'numeric', required: false }
+  },
+  elements: {
+    label:   { type: 'string' , required: true , multi: false },
+    layout:  { type: 'layout' , required: false, multi: false }
+  },
+  content: false
+});
