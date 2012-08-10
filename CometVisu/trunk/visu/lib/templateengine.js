1e675f62155f1b9b71e7bb6fa515554a5205d178
peuter <peuter@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2012-08-10 17:46:32 +0000

    fix to make the pagejumps work with the new page IDs (committed in Rev 932)

--- CometVisu/trunk/visu/lib/templateengine.js
+++ CometVisu/trunk/visu/lib/templateengine.js
@@ -550,7 +550,7 @@ function create_pages( page, path, flavour, type ) {
 function scrollToPage( page_id, speed ) {
   $('.activePage').removeClass('activePage');
   $('.pagejump.active').removeClass('active');
-  if (page_id.match(/^[0-9_]+$/)==null) {
+  if (page_id.match(/^id_[0-9_]+$/)==null) {
 	  // find Page-ID by name
 	  $('.page h1').each(function(i) {
 		  if ($(this).text()==page_id) {
@@ -589,7 +589,7 @@ function scrollToPage( page_id, speed ) {
   // set pagejump for this page to active if it exists
   $(".pagejump > .actor").each(function (i) {
 	  var data = $(this).data();
-	  if (data.target.match(/^[0-9_]+$/)==null) {
+	  if (data.target.match(/^id_[0-9_]+$/)==null) {
 		  // get page id by name
 		  if ($('#'+page_id+' h1').text()==data.target) {
 			  $(this).parent().addClass("active");
