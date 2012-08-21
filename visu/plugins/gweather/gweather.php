a91c6efcbd4b892a279e4041363368d287e489b7
makki1 <Makki1@users.noreply.github.com>
2012-08-21 13:35:54 +0000

    Make gweather more robust - fixes #3558064

--- visu/plugins/gweather/gweather.php
+++ visu/plugins/gweather/gweather.php
@@ -2,7 +2,14 @@
 header("content-type: text/xml");
 $city = $_GET['city'];
 $lang = $_GET['lang'];
+$xmlData = '';
+$retries = 0;
 
-$xmlData = utf8_encode(file_get_contents("http://www.google.com/ig/api?hl=$lang&weather=$city"));
+// ugly workaround for google returning crap sometimes
+while (($xmlData == "Unsupported API" or $xmlData == "") and $retries < 5) {
+    $retries++;
+    $xmlData = utf8_encode(file_get_contents("http://www.google.com/ig/api?hl=$lang&weather=$city"));
+}
 echo $xmlData;
 ?>
+
