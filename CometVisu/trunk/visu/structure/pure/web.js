bac2bacb95773ad7b6b7f4699b55a55ed9b4fdc9
netzkind <netzkind@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2012-12-26 08:38:07 +0000

    widget 'iframe' is now 'web'; xsd is already up to speed.

--- CometVisu/trunk/visu/structure/pure/web.js
+++ CometVisu/trunk/visu/structure/pure/web.js
@@ -1,4 +1,4 @@
-/* iframe.js (c) 2012 by Christian Mayer [CometVisu at ChristianMayer dot de]
+/* web.js (c) 2012 by Christian Mayer [CometVisu at ChristianMayer dot de]
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -15,27 +15,27 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
-basicdesign.addCreator('iframe', {
+basicdesign.addCreator('web', {
   create: function( element, path, flavour, type ) {
     var $e = $(element);
     var layout = $e.children('layout')[0];
     var style = layout ? 'style="' + extractLayout( layout, type ) + '"' : '';
-    var ret_val = $('<div class="widget iframe" ' + style + '/>');
+    var ret_val = $('<div class="widget web" ' + style + '/>');
     ret_val.setWidgetLayout($e);
     if( $e.attr('flavour') ) flavour = $e.attr('flavour');// sub design choice
     if( flavour ) ret_val.addClass( 'flavour_' + flavour );
     ret_val.append( extractLabel( $e.find('label')[0] ) );
-    var iframeStyle = '';
+    var webStyle = '';
     if( $e.attr('width' ) ) {
-      iframeStyle += 'width:'  + $e.attr('width' ) + ';'; 
+      webStyle += 'width:'  + $e.attr('width' ) + ';'; 
     } else {  // default width is 100% of widget space (fix bug #3175343 part 1)
-      iframeStyle += 'width: 100%;';
+      webStyle += 'width: 100%;';
     }
-    if( $e.attr('height') ) iframeStyle += 'height:' + $e.attr('height') + ';';
+    if( $e.attr('height') ) webStyle += 'height:' + $e.attr('height') + ';';
     if( $e.attr('frameborder') == 'false' ) style += 'border: 0px ;';
-    if( $e.attr('background') ) iframeStyle += 'background-color:' + $e.attr('background') + ';';
-    if( iframeStyle != '' ) iframeStyle = 'style="' + iframeStyle + '"';
-    var actor = '<div class="actor"><iframe src="' +$e.attr('src') + '" ' + iframeStyle + '></iframe></div>';
+    if( $e.attr('background') ) webStyle += 'background-color:' + $e.attr('background') + ';';
+    if( webStyle != '' ) webStyle = 'style="' + webStyle + '"';
+    var actor = '<div class="actor"><iframe src="' +$e.attr('src') + '" ' + webStyle + '></iframe></div>';
     
     var refresh = $e.attr('refresh') ? $e.attr('refresh')*1000 : 0;
     ret_val.append( $(actor).data( {
