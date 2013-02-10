56ccad26ad08eafee64d64f097b0a8743fa257ac
Christian Mayer <ChristianMayer@users.noreply.github.com>
2013-02-10 20:32:02 +0000

    Little bug fix for the cases when an hash tag is in the URL

--- visu/lib/compatibility.js
+++ visu/lib/compatibility.js
@@ -165,13 +165,14 @@ sprintf = sprintfWrapper.init;
 $.extend({
   getUrlVars: function(){
     var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('#')[0].split('&');
     for(var i = 0; i < hashes.length; i++)
     {
       hash = hashes[i].split('=');
       vars.push(hash[0]);
       vars[hash[0]] = hash[1];
     }
+    console.log( vars );
     return vars;
   },
   getUrlVar: function(name){
