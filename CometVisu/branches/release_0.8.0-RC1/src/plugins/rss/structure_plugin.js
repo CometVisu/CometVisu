1ad4d2e327442748759cb98196c7d6ea391b9387
mayerch <mayerch@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2013-10-20 14:30:37 +0000

    Creating release branch 0.8.0-RC1

---
+++ CometVisu/branches/release_0.8.0-RC1/src/plugins/rss/structure_plugin.js
@@ -0,0 +1,109 @@
+/* structure_plugin.js (c) 2011 by Michael Markstaller [devel@wiregate.de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+*/
+
+/**
+ * This plugins integrates zrssfeed to display RSS-Feeds via Google-API 
+ * *and* a parser for local feeds using jQuery 1.5+ into CometVisu.
+ * rssfeedlocal is derived from simplerss and zrssfeed
+ * rssfeedlocal is mainly meant to be used with rsslog.php and plugins
+ * Examples
+ *   <rss src="/visu/plugins/rss/rsslog.php" refresh="300" link="false" title="false"></rss>
+ *   <rss src="http://www.tagesschau.de/xml/rss2" refresh="300">Test API</rss>
+ *   <rss src="/visu/plugins/rss/tagesschau-rss2.xml" refresh="300" header="true" date="true"></rss>
+ */
+
+$.includeScripts([
+  'plugins/rss/zrssfeed/jquery.zrssfeed.js'
+], templateEngine.pluginLoaded );
+
+VisuDesign_Custom.prototype.addCreator("rss", {
+    create: function( page, path ) {
+        var $p = $(page);
+
+        function uniqid() {
+            var newDate = new Date;
+            return newDate.getTime();
+        }
+
+        var id = "rss_" + uniqid();
+
+        var ret_val = $('<div class="widget clearfix rss" />');
+        basicdesign.setWidgetLayout( ret_val, $p );
+        var label = '<div class="label">' + page.textContent + '</div>';
+        var actor = $("<div class=\"actor\"><div class=\"rss_inline\" id=\"" + id + "\"></div>");
+        var rss = $("#" + id, actor);
+
+        if ($p.attr("width")) {
+            rss.css("width", $p.attr("width"));
+        }
+        if ($p.attr("height")) {
+            rss.css("height", $p.attr("height"));
+        }
+
+        ret_val.append(label).append(actor);
+
+        rss.data("id", id);
+        rss.data("src", $p.attr("src"));
+        rss.data("label", page.textContent);
+        rss.data("refresh", $p.attr("refresh"));
+        rss.data("limit", $p.attr("limit")) || 10;
+        rss.data("header", $p.attr("header")) || true;
+        rss.data("date", $p.attr("date")) || true;
+        rss.data("content", $p.attr("content")) || true;
+        rss.data("snippet", $p.attr("snippet")) || true;
+        rss.data("showerror", $p.attr("showerror")) || true;
+        rss.data("ssl", $p.attr("ssl")) || false;
+        rss.data("linktarget", $p.attr("linktarget")) || "_new";
+        rss.data("link", $p.attr("link")) || true;
+        rss.data("title", $p.attr("title")) || true;
+        
+        refreshRSS(rss, {});
+
+        return ret_val;
+    }
+});
+
+function refreshRSS(rss, data) {
+    var rss = $(rss);
+
+    var src = rss.data("src");
+    var label = rss.data("label");
+    var refresh = rss.data("refresh");
+    var limit = rss.data("limit");
+    //FIXME: eval really needed?? to convert string true/false to bool?
+    
+        jQuery(function() {
+          $(rss).rssfeed(src, {
+            limit: rss.data("limit"),
+            header: eval(rss.data("header")),
+            date: eval(rss.data("date")),
+            content: rss.data("content"),
+            snippet: eval(rss.data("snippet")),
+            showerror: eval(rss.data("showerror")),
+            ssl: eval(rss.data("ssl")),
+            linktarget: rss.data("linktarget"),
+          });
+        });
+        if (typeof (refresh) != "undefined" && refresh) {
+      // reload regularly
+      window.setTimeout(function(rss, data) {
+        refreshRSS(rss, data)
+      }, refresh * 1000, rss, data);
+    }
+    //rss.data("itemoffset") = itemoffset;
+    return false;
+}
---
+++ CometVisu/branches/release_0.8.0-RC1/src/structure/pure/infotrigger.js
@@ -0,0 +1,135 @@
+/* infotrigger.js (c) 2012 by Christian Mayer [CometVisu at ChristianMayer dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+basicdesign.addCreator('infotrigger', {
+  create: function( element, path, flavour, type ) {
+    var $e = $(element);
+    
+    // create the main structure
+    var makeAddressListFn = function( src, transform, mode, variant ) {
+      return [ true, variant == 'short' ? 1 : 0 ];
+    }
+    var ret_val = basicdesign.createDefaultWidget( 'infotrigger', $e, path, flavour, type, this.update, makeAddressListFn );
+    // and fill in widget specific data
+    ret_val.data( {
+      'downvalue'     : $e.attr('downvalue' )            || 0,
+      'shortdownvalue': $e.attr('shortdownvalue')        || 0,
+      'downlabel'     : $e.attr('downlabel')                 ,
+      'upvalue'       : $e.attr('upvalue' )              || 0,
+      'shortupvalue'  : $e.attr('shortupvalue')          || 0,
+      'uplabel'       : $e.attr('uplabel')                   ,
+      'shorttime'     : parseFloat($e.attr('shorttime')) || -1,
+      'isAbsolute'    : ($e.attr('change')               || 'relative') == 'absolute',
+      'min'           : parseFloat($e.attr('min'))       || 0,
+      'max'           : parseFloat($e.attr('max'))       || 255,
+      'format'        : $e.attr('format')
+    } );
+    var data = ret_val.data();
+
+    // create buttons + info
+    var buttons = $('<div style="float:left;"/>');
+
+    var actordown = '<div class="actor switchUnpressed downlabel" ';
+    if ( data.align ) 
+      actordown += 'style="text-align: ' + data.align + '" '; 
+    actordown += '>';
+    actordown += '<div class="label">' + (data.downlabel || '-') + '</div>';
+    actordown += '</div>';
+    var $actordown = $(actordown).data({
+      'value'     : data.downvalue,
+      'shortvalue': data.shortdownvalue
+    });
+    basicdesign.createDefaultButtonAction( $actordown, $actordown, this.downaction, this.action );
+
+    var actorup = '<div class="actor switchUnpressed uplabel" ';
+    if ( data.align ) 
+      actorup += 'style="text-align: ' + data.align + '" '; 
+    actorup += '>';
+    actorup += '<div class="label">' + (data.uplabel || '+') + '</div>';
+    actorup += '</div>';
+    var $actorup = $(actorup).data({
+      'value'     : data.upvalue,
+      'shortvalue': data.shortupvalue
+    });
+    basicdesign.createDefaultButtonAction( $actorup, $actorup, this.downaction, this.action );
+
+    var actorinfo = '<div class="actor switchInvisible " ';
+    if ( data.align ) 
+      actorinfo += 'style="text-align: ' + data.align + '" '; 
+    actorinfo += '" ><div class="value"></div></div>';
+    var $actorinfo = $(actorinfo);
+
+    switch ($e.attr('infoposition')) {
+      case 'middle':
+        buttons.append( $actordown );
+        buttons.append( $actorinfo );
+        buttons.append( $actorup );        
+        break;
+      case 'right':
+        buttons.append( $actordown );
+        buttons.append( $actorup );        
+        buttons.append( $actorinfo );
+        break;
+      default:
+        buttons.append( $actorinfo );
+        buttons.append( $actordown );
+        buttons.append( $actorup );        
+        break;
+    }
+
+    ret_val.append( buttons );
+
+    // initially setting a value
+    basicdesign.defaultUpdate(undefined, undefined, ret_val, true);
+
+    return ret_val;
+  },
+
+  update: function(e,d) { 
+    var element = $(this);
+    var value = basicdesign.defaultUpdate( e, d, element, true );
+  },
+  downaction: function(event) {
+    $(this).parent().parent().data( 'downtime', new Date().getTime() );
+  },
+  action: function(event) {
+    var $this      = $(this),
+        buttonData = $this.data(),
+        data       = $this.parent().parent().data();
+    if( data.downtime )
+    {
+      var isShort = (new Date().getTime()) - data.downtime < data.shorttime;
+      var value = isShort ? buttonData.shortvalue : buttonData.value;
+      if( data.isAbsolute )
+      {
+        value = parseFloat(data.basicvalue);
+        if( isNaN( value ) )
+          value = 0; // anything is better than NaN...
+        value = value + parseFloat(isShort ? buttonData.shortvalue : buttonData.value);
+        if (value < data.min ) value = data.min;
+        if( value > data.max ) value = data.max;
+      }
+      for( var addr in data.address )
+      {
+        if( !(data.address[addr][1] & 2) ) continue; // skip when write flag not set
+        if ((isShort && data.address[addr][2] == 1) || (!isShort && data.address[addr][2] == 0)) {
+          templateEngine.visu.write( addr.substr(1), templateEngine.transformEncode( data.address[addr][0], value ) );
+        }
+      }
+    }
+  }
+});
