3836dadd09e0f7210723d8e9a0db5c4e668a8880
mayerch <mayerch@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2013-11-10 22:14:46 +0000

    Update path to new demo config directory

--- CometVisu/_support/make_release.sh
+++ CometVisu/_support/make_release.sh
@@ -45,7 +45,7 @@ fi
 $SVN_CMD up
 echo $VERSION > $RELEASE_DIR/VERSION
 sed -i "s/Version: SVN/Version: $VERSION/" $RELEASE_DIR/src/config/visu_config.xml 
-sed -i "s/Version: SVN/Version: $VERSION/" $RELEASE_DIR/src/config/visu_config_demo.xml 
+sed -i "s/Version: SVN/Version: $VERSION/" $RELEASE_DIR/src/config/demo/visu_config_demo.xml 
 sed -i "s/comet_16x16_000000.png/comet_16x16_ff8000.png/" $RELEASE_DIR/src/index.html
 cd $RELEASE_DIR
 make
