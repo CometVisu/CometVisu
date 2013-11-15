79f658f417ce0fb65d264e9fd80d955897eeb886
hausl <hausl@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2013-11-15 01:25:43 +0000

    bugfix page-widget with missing backdrop-attribute

--- src/structure/pure/page.js
+++ src/structure/pure/page.js
@@ -101,7 +101,7 @@ basicdesign.addCreator('page', {
       if( $p.attr('size') == 'fixed' )
         size = '';
       // else: assume scaled
-      var elemType = '.svg' == backdrop.substring( backdrop.length - 4 ) ? 'embed' : 'img';
+      var elemType = (undefined != backdrop && '.svg' == backdrop.substring( backdrop.length - 4 )) ? 'embed' : 'img';
       container.append( '<' + elemType + ' src="' + backdrop + '" style="position: absolute; top: 0px; left: 0px;z-index:-1;' + size + '"/>' );
     } else if( '3d' == type && false ) //---Disable 3D for 0.8---
     {
