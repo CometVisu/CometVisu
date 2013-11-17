5b665aae42c220d53a75ee07ca4e7614838195ef
Christian Mayer <ChristianMayer@users.noreply.github.com>
2013-11-17 21:45:51 +0000

    New release: 0.8.0

---
+++ release/lib/library_version.inc.php
@@ -0,0 +1,37 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * define the current version of the library
+ *
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @category    visu
+ * @package     CometVisu
+ * @author      Julian Makowski (julian at makowskis dot de)
+ * @copyright   2013 Julian Makowski
+ * @license     GPLv3 or later, http://opensource.org/licenses/gpl-license.php
+ * @version     SVN: $Id$
+ * @link        http://cometvisu.de
+ * @since       2013-02-05
+ */
+
+/**
+ * the current lib version
+ * @const   integer
+ */
+define('LIBRARY_VERSION', 5);
+
+?>
