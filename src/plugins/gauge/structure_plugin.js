fe3e4d32b8f4d2ba3024f2c58a8be54cc7e5ded1
netfritz <netfritz@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2014-01-28 15:21:56 +0000

    plugin gauge hinzugefuegr

---
+++ src/plugins/gauge/structure_plugin.js
@@ -0,0 +1,92 @@
+/* structure_custom.js (c) 01/2014 by NetFritz [NetFritz at gmx dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+$.includeScripts([
+  'plugins/gauge/lib/tween-min.js',
+  'plugins/gauge/lib/steelseries-min.js'
+], templateEngine.pluginLoaded );
+
+VisuDesign_Custom.prototype.addCreator("gauge", {
+    create: function( page, path, element,type ) {
+        var $p = $(page);
+	    var $e = $(element);
+		console.log($e);
+        // create the main structure
+        var ret_val = basicdesign.createDefaultWidget( 'gauge', $e, path, type, this.update );
+
+        function uniqid() {
+            var newDate = new Date;
+            return newDate.getTime();
+        }
+        var id = "gauge_" + uniqid();
+        var radialid = uniqid();
+		var radial = {};
+		
+    	// and fill in widget specific data	
+        ret_val.data( {
+		        'type' : $p.attr('type'),
+         'titleString' : $p.attr('titleString'),
+		  'unitString' : $p.attr('unitString'),
+		    'minValue' : $p.attr('minValue'),
+		    'maxValue' : $p.attr('maxValue'),
+          'lcdVisible' : $p.attr('lcdVisible' ) || 'false',
+	            'size' : $p.attr('size'),
+             'format'  : $p.attr('format')
+        } );	
+		var data = ret_val.data();
+		var titleString = data.titleString;	
+        var type = data.type;	
+		var size = data.size;
+        var unitString = data.unitString;	
+		var lcdVisible = data.lcdVisible;	
+		var minValue = data.minValue;
+		var maxValue = data.maxValue;
+var address = basicdesign.makeAddressList($p);
+console.log("adress=",address);
+	    // create the actor
+		var $actor = $("<canvas id=" + id + "></canvas>");
+        ret_val.append( $actor ); 
+
+    	templateEngine.bindActionForLoadingFinished(function() {
+	        radial[radialid] = new steelseries[type](id, {			
+                     titleString : [titleString],
+					  unitString : [unitString],
+					        size : [size],
+                       lcdVisibl : [lcdVisible]
+		    });			
+            if (type == 'Radial') {
+		       radial[radialid].setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+	           radial[radialid].setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+	           radial[radialid].setForegroundType(steelseries.ForegroundType.TYPE1);
+	           radial[radialid].setPointerColor(steelseries.ColorDef.RED);
+	           //radial[radialid].setPointerTypeAverage(steelseries.PointerType.TYPE1);
+		       radial[radialid].setMinValue(minValue);
+			   radial[radialid].setMaxValue(maxValue);
+		       radial[radialid].setValueAnimated(50);
+            } else if(type == 'WindDirection'){
+    	    	radial[radialid].setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+		        radial[radialid].setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+		        radial[radialid].setForegroundType(steelseries.ForegroundType.TYPE1);
+		        radial[radialid].setPointerColor(steelseries.ColorDef.RED);
+		        radial[radialid].setPointerTypeAverage(steelseries.PointerType.TYPE1);
+				radial[radialid].setValueAnimatedLatest(80);
+				radial[radialid].setValueAnimatedAverage(90);
+            }
+        });
+     	return ret_val;	
+	}
+});		
