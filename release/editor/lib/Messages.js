dca63b373e6f0206c5729cf02f9d0c97f8fe8010
Christian Mayer <ChristianMayer@users.noreply.github.com>
2014-02-08 16:26:28 +0000

    New release: 0.8.1

---
+++ release/editor/lib/Messages.js
@@ -0,0 +1,123 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Listing of messages used throughout the editor in user-related information.
+ * 
+ * Might be the place for i18n
+ *
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @category    editor
+ * @package     CometVisu
+ * @author      Julian Makowski (julian at makowskis dot de)
+ * @copyright   2012 Julian Makowski
+ * @license     GPLv3 or later, http://opensource.org/licenses/gpl-license.php
+ * @version     SVN: $Id$
+ * @link        http://cometvisu.de
+ * @since       2012-11-21
+ */
+
+var Messages = {
+    language: 'en',
+    validity: {
+        configurationInvalid: 'The configuration appears to be not valid. ' +
+                                'Please check with \'check_config.php\' for details.\n' +
+                                'Error: \'%s\'',
+        valueInvalid: 'This is not a valid value.',
+        valueInvalidForType: 'This is not a valid value, expected \'%s\'',
+        noMoreChildrenAllowed: 'This element allows for no more child-elements.',
+    },
+    loader: {
+        loading: 'Loading and validating configuration, this may take several seconds...',
+    },
+    configuration: {
+        filenameInvalid: 'no, empty or invalid filename given, can not instantiate without one',
+        loadingError: 'configuration could not be loaded, \'%s\' / \'%s\'',
+        savingError: 'configuration could not be saved, server responded with \'%s\'',
+        savingErrorUnknown: 'configuration could not be saved, server-response is not understood',
+        savingErrorServer: 'configuration could not be saved, server responded with \'%s\' / \'%s\'',
+        saved: 'configuration was saved.',
+        schemaNotFound: 'no schema/xsd found in root-level-element, can not run without one',
+    },
+    dataProvider: {
+        loadingError: 'data-provider could not be loaded, \'%s\' / \'%s\'',
+    },
+    editor: {
+        elementNotRemovable: 'This element can not be removed',
+        elementNotCuttable: 'This element can not be cut - please use copy instead',
+        notSavingInvalidConfiguration: 'The configuration is not valid, and as such can not be saved.',
+        ui: {
+            disabled: {
+                tooltip: 'not avilable',
+                text: undefined,
+            },
+            addChild: {
+                tooltip: 'Add an element as a child',
+                text: 'add child',
+            },
+            remove: {
+                tooltip: 'Remove this element and all of its children',
+                text: 'remove',
+            },
+            attributes: {
+                tooltip: 'Display attributes for this element',
+                text: undefined,
+            },
+            children: {
+                tooltip: 'Toggle display of child-elements for this element',
+                text: undefined,
+            },
+            cut: {
+                tooltip: 'cut',
+                text: 'cut',
+            },
+            copy: {
+                tooltip: 'copy',
+                text: 'copy',
+            },
+            paste: {
+                tooltip: 'paste',
+                text: 'paste',
+            },
+            sort: {
+                tooltip: 'sort',
+                text: 'sort',
+            },
+            toggleSubmenu: {
+                tooltip: 'Toggle display of submenu/action-menu',
+                text: undefined,
+            },
+            clickToEdit: {
+                tooltip: 'click to edit',
+                text: undefined,
+            },
+            save: {
+                tooltip: 'validate config and save',
+                text: 'save',
+            },
+            preview: {
+                tooltip: 'validate config, save it temporarily, and preview it',
+                text: 'preview',
+            },
+            expert: {
+                tooltip: 'show/hide attributes for advanced configuration',
+                text: 'complex',
+            },
+        },
+    },
+    schema: {
+        complexType: 'complex type, please see documentation',
+    },
+};
---
+++ release/plugins/gauge/structure_plugin.js
@@ -0,0 +1,110 @@
+/* structure_custom.js (c) 01/2014 by NetFritz [NetFritz at gmx dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+ 
+$.includeScripts([
+  'plugins/gauge/lib/tween-min.js',
+  'plugins/gauge/lib/steelseries-min.js'
+], templateEngine.pluginLoaded );
+
+VisuDesign_Custom.prototype.addCreator("gauge", {
+    create: function( element, path, flavour, type ) {
+        var $e = $(element);
+        // create the main structure
+        var ret_val = basicdesign.createDefaultWidget( 'gauge', $e, path, flavour, type, this.update, function( src, transform, mode, variant ) {
+          return [ true, variant ];
+        });     
+        var id = "gauge_" + path;
+
+        // and fill in widget specific data
+        ret_val.data( {
+           'type' : $e.attr('type'),
+           'titleString' : $e.attr('titleString') || '', 
+           'unitString'  : $e.attr('unitString') || '', 
+           'minValue'    : $e.attr('minValue') || 0, 
+           'maxValue'    : $e.attr('maxValue') || 100,
+           'lcdVisible'  : $e.attr('lcdVisible') || false,
+           radial        : undefined,
+           'size'        : $e.attr('size') || '150', 
+		       'threshold'   : $e.attr('threshold'),  
+           'format'      : $e.attr('format')
+        });
+        var data = ret_val.data();
+        var titleString = data.titleString;
+        var type = data.type;
+        var size = data.size;
+        var unitString = data.unitString;
+        var lcdVisible = data.lcdVisible;
+        var minValue = data.minValue;
+        var maxValue = data.maxValue;
+        var threshold = data.threshold;
+
+        // create the actor
+        //var $actor = $('div class="actor"><div class="value"></div></div><canvas id=' + id + '></canvas>');  
+        var $actor = $('div class="actor"></div><canvas id=' + id + '></canvas>');		
+        ret_val.append( $actor ); 
+        basicdesign.defaultUpdate(undefined, undefined, ret_val, true);
+
+        templateEngine.bindActionForLoadingFinished(function() {
+              var radial = new steelseries[type](id, {
+                     titleString : [titleString],
+                      unitString : [unitString],
+                            size : [size],
+                      lcdVisible : [lcdVisible]
+              });
+
+              if (type == 'Radial') {
+                   radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+                   radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+                   radial.setForegroundType(steelseries.ForegroundType.TYPE1);
+                   radial.setPointerColor(steelseries.ColorDef.RED);
+                   radial.setPointerType(steelseries.PointerType.TYPE1);
+                   radial.setMinValue(minValue);
+                   radial.setMaxValue(maxValue);
+                   radial.setThreshold(threshold);				   
+                   //radial.setValueAnimated(1000);
+              } else if(type == 'WindDirection'){
+                   radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+                   radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+                   radial.setForegroundType(steelseries.ForegroundType.TYPE1);
+                   radial.setPointerColor(steelseries.ColorDef.RED);
+                   radial.setPointerTypeAverage(steelseries.PointerType.TYPE1);
+                   //radial.setValueAnimatedLatest(80);
+                   //radial.setValueAnimatedAverage(90);
+              }
+              ret_val.data( 'radial', radial );
+        });
+        return ret_val;	
+    },
+    update: function(e,d) { 
+      var element = $(this);
+      var value = basicdesign.defaultUpdate( e, d, element, true );
+      var variant = element.data( 'address' )[ e.type ][2];
+      switch( variant )
+      {
+        case 'average':
+          if( element.data('radial') && element.data('radial').setValueAnimatedAverage )
+            element.data('radial').setValueAnimatedAverage( value );
+          break;
+
+        default:
+          if( element.data('radial') && element.data('radial').setValueAnimatedLatest )
+            element.data('radial').setValueAnimatedLatest( value );
+          if( element.data('radial') && element.data('radial').setValueAnimated )
+            element.data('radial').setValueAnimated( value );
+      }
+   }
+});
