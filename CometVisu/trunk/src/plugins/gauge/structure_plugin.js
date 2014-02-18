d88bfadac322d2879136c66999c7e4eb9c04290a
netfritz <netfritz@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2014-02-18 03:33:40 +0000

    Gauge structure_plugin.js trend_visible

--- CometVisu/trunk/src/plugins/gauge/structure_plugin.js
+++ CometVisu/trunk/src/plugins/gauge/structure_plugin.js
@@ -1,4 +1,4 @@
-/* structure_custom.js (c) 01/2014 by NetFritz [NetFritz at gmx dot de]
+﻿/* structure_custom.js (c) 01/2014 by NetFritz [NetFritz at gmx dot de]
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -15,6 +15,28 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
  
+ /*
+* This plugins integrates Gauge (in canavas) into the visualization
+*
+*
+* short documentation
+*
+* recommended widgets:
+* - WindDirection
+* - Radial
+*
+* attributes:
+* - titleString: optional, "name"
+* - unitString: optional, "units"
+* - minValue: optional, "measuring range"
+* - maxValue: optional, "measuring range"
+* - lcdVisible: optional, "true", "false" 
+* - trendVisible: optional, "true" , "false"
+* - size: optional, preset "150" 
+* - threshold: optional, ""
+* - format: optional, ""
+*
+*/ 
 $.includeScripts([
   'plugins/gauge/lib/tween-min.js',
   'plugins/gauge/lib/steelseries-min.js'
@@ -32,14 +54,15 @@ VisuDesign_Custom.prototype.addCreator("gauge", {
         // and fill in widget specific data
         ret_val.data( {
            'type' : $e.attr('type'),
-           'titleString' : $e.attr('titleString') || '', 
-           'unitString'  : $e.attr('unitString') || '', 
+           'titleString' : $e.attr('titleString') || '',
+           'unitString'  : $e.attr('unitString') || '',
            'minValue'    : $e.attr('minValue') || 0, 
            'maxValue'    : $e.attr('maxValue') || 100,
            'lcdVisible'  : $e.attr('lcdVisible') || false,
+           'trendVisible': $e.attr('trendVisible') || false,
            radial        : undefined,
-           'size'        : $e.attr('size') || '150', 
-		       'threshold'   : $e.attr('threshold'),  
+           'size'        : $e.attr('size') || '150',
+           'threshold'   : $e.attr('threshold'),
            'format'      : $e.attr('format')
         });
         var data = ret_val.data();
@@ -47,64 +70,76 @@ VisuDesign_Custom.prototype.addCreator("gauge", {
         var type = data.type;
         var size = data.size;
         var unitString = data.unitString;
-        var lcdVisible = data.lcdVisible;
         var minValue = data.minValue;
         var maxValue = data.maxValue;
         var threshold = data.threshold;
-
-        // create the actor
-        //var $actor = $('div class="actor"><div class="value"></div></div><canvas id=' + id + '></canvas>');  
-        var $actor = $('div class="actor"></div><canvas id=' + id + '></canvas>');		
+        if(data.lcdVisible == 'false') var lcdVisible = false;
+        else if(data.lcdVisible == 'true') var lcdVisible = true;
+        if(data.trendVisible == 'false') var trendVisible = false;
+        if(data.trendVisible == 'true') var trendVisible = true;
+        // create the actor 
+        var $actor = $('div class="actor"></div><canvas id=' + id + '></canvas>');
         ret_val.append( $actor ); 
         basicdesign.defaultUpdate(undefined, undefined, ret_val, true);
 
         templateEngine.bindActionForLoadingFinished(function() {
-              var radial = new steelseries[type](id, {
+            if (type == 'Radial') { 
+                var radial = new steelseries[type](id, {
                      titleString : [titleString],
                       unitString : [unitString],
                             size : [size],
-                      lcdVisible : [lcdVisible]
-              });
-
-              if (type == 'Radial') {
-                   radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
-                   radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
-                   radial.setForegroundType(steelseries.ForegroundType.TYPE1);
-                   radial.setPointerColor(steelseries.ColorDef.RED);
-                   radial.setPointerType(steelseries.PointerType.TYPE1);
-                   radial.setMinValue(minValue);
-                   radial.setMaxValue(maxValue);
-                   radial.setThreshold(threshold);				   
+                      lcdVisible : lcdVisible,
+                    trendVisible : trendVisible
+                });
+                radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+                radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+                radial.setForegroundType(steelseries.ForegroundType.TYPE1);
+                radial.setPointerColor(steelseries.ColorDef.RED);
+                radial.setPointerType(steelseries.PointerType.TYPE1);
+                radial.setMinValue(minValue);
+                radial.setMaxValue(maxValue);
+                radial.setThreshold(threshold);
                    //radial.setValueAnimated(1000);
-              } else if(type == 'WindDirection'){
-                   radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
-                   radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
-                   radial.setForegroundType(steelseries.ForegroundType.TYPE1);
-                   radial.setPointerColor(steelseries.ColorDef.RED);
-                   radial.setPointerTypeAverage(steelseries.PointerType.TYPE1);
-                   //radial.setValueAnimatedLatest(80);
-                   //radial.setValueAnimatedAverage(90);
-              }
-              ret_val.data( 'radial', radial );
+            } else if(type == 'WindDirection'){
+                var radial = new steelseries[type](id, {
+                     titleString : [titleString],
+                      unitString : [unitString],
+                            size : [size],
+                    //lcdVisible : [lcdVisible]
+                });
+                radial.setFrameDesign(steelseries.FrameDesign.BLACK_METAL);
+                radial.setBackgroundColor(steelseries.BackgroundColor.DARK_GRAY);
+                radial.setForegroundType(steelseries.ForegroundType.TYPE1);
+                radial.setPointerColor(steelseries.ColorDef.RED);
+                radial.setPointerTypeAverage(steelseries.PointerType.TYPE1);
+                //radial.setValueAnimatedLatest(80);
+                //radial.setValueAnimatedAverage(90);
+            }
+            ret_val.data( 'radial', radial );
         });
         return ret_val;	
     },
-    update: function(e,d) { 
-      var element = $(this);
-      var value = basicdesign.defaultUpdate( e, d, element, true );
-      var variant = element.data( 'address' )[ e.type ][2];
-      switch( variant )
-      {
+    update: function(e,d) {
+        var element = $(this);
+        var value = basicdesign.defaultUpdate( e, d, element, true );
+        var variant = element.data( 'address' )[ e.type ][2];
+        switch( variant ){
         case 'average':
-          if( element.data('radial') && element.data('radial').setValueAnimatedAverage )
-            element.data('radial').setValueAnimatedAverage( value );
-          break;
-
+            if( element.data('radial') && element.data('radial').setValueAnimatedAverage )
+                element.data('radial').setValueAnimatedAverage( value );
+            break;
+        case 'trend':
+           if( element.data('radial') && element.data('radial').setTrend ){
+                if ( value > 0) element.data('radial').setTrend(steelseries.TrendState.UP);
+                else if (value < 0) element.data('radial').setTrend(steelseries.TrendState.DOWN);
+                else element.data('radial').setTrend(steelseries.TrendState.STEADY);
+            }
+        break;
         default:
-          if( element.data('radial') && element.data('radial').setValueAnimatedLatest )
-            element.data('radial').setValueAnimatedLatest( value );
-          if( element.data('radial') && element.data('radial').setValueAnimated )
-            element.data('radial').setValueAnimated( value );
-      }
-   }
+            if( element.data('radial') && element.data('radial').setValueAnimatedLatest )
+               element.data('radial').setValueAnimatedLatest( value );
+            if( element.data('radial') && element.data('radial').setValueAnimated )
+               element.data('radial').setValueAnimated( value );
+        }
+    }
 });
