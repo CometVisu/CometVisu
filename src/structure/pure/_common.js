8f3ec4a8d3536ebbd9d8188b6789b2a5b1174c5c
Tobias Bräutigam <tbraeutigam@gmail.com>
2015-04-09 09:49:55 +0200

    - added default mapping to metal-config - fix for default value handling (fixes #153)

--- src/structure/pure/_common.js
+++ src/structure/pure/_common.js
@@ -396,7 +396,12 @@ function VisuDesign() {
       'path'    : path
     });
     var ret_val = '<div class="'+classes+'" ' + style + '>' + label;
-    
+    if (address) {
+      templateEngine.bindActionForLoadingFinished(function() {
+        // initially setting a value
+        basicdesign.defaultUpdate( undefined, undefined, $("#"+path), true, path );
+      });
+    }
     return ret_val;
   };
   
