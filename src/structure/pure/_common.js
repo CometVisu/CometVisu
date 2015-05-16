ba8a9005e9283031ef94b10d420830a3cda4e6af
Tobias Bräutigam <tbraeutigam@gmail.com>
2015-05-16 13:43:27 +0200

    added some missing var declarations

--- src/structure/pure/_common.js
+++ src/structure/pure/_common.js
@@ -481,7 +481,7 @@ function placementStrategy( anchor, popup, page, preference )
   var position_order = [ 8, 2, 6, 4, 9, 3, 7, 1, 5, 0 ];
   if( preference !== undefined ) position_order.unshift( preference );
   
-  for( pos in position_order )
+  for( var pos in position_order )
   {
     var xy = {};
     switch(position_order[pos])
