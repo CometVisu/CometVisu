c390668bb5a04e83be27dbeefdd5c33fc24f18b1
Stefan Borchert <stefan.borchert@undpaul.de>
2015-10-11 14:54:07 +0200

    PR #191: update author information for link plugin.

--- src/plugins/link/structure_plugin.js
+++ src/plugins/link/structure_plugin.js
@@ -1,4 +1,4 @@
-/* structure_plugin.js (c) 2012 by Michael Hausl [michael@hausl.com]
+/* structure_plugin.js (c) 2015 by Stefan Borchert [stefan@borchert.cc]
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
