030b702328ac543e96aa7ae26ca2e994326f8789
ChristianMayer <mail@christianmayer.de>
2016-02-19 21:57:44 +0100

    Syntax fix

--- build/travis_codestyle_check.sh
+++ build/travis_codestyle_check.sh
@@ -3,4 +3,4 @@ set -ev
 
 # Test for lines starting with a tabulator
 # The use of tabulators is violating the coding style. Please use spaces instead 
-!grep -rnaP '^\t' --include='*.js' --exclude-dir=dependencies --exclude-dir='dep' src/
+! grep -rnaP '^\t' --include='*.js' --exclude-dir=dependencies --exclude-dir='dep' src/
