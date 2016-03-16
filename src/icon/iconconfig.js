b0b41058f805511253c76ac43c41bf56a495d45b
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-03-16 20:23:57 +0100

    indentation fix

--- src/icon/iconconfig.js
+++ src/icon/iconconfig.js
@@ -16,85 +16,85 @@
  */
 
 define( ['lib/icontools'], function() {
-    "use strict";
-    /**
-     * Internal database of the known icons.
-     * Initially filled with the default icons.
-     */
-    return {
-        'CometVisu' : { '*' : '128',
-            '16' : {
-                'black'    : '*/000000',
-                'white'    : '*/ffffff',
-                'antimony' : '*/00ddff',
-                'boron'    : '*/00ff11',
-                'lithium'  : '*/ff0000',
-                'potassium': '*/d00055',
-                'orange  ' : '*/ff8000',
-                '*': { '*' : 'ff8000',
-                    '000000': { 'uri': 'icon/comet_16_000000.png'},
-                    'ffffff': { 'uri': 'icon/comet_16_ffffff.png'},
-                    '00ddff': { 'uri': 'icon/comet_16_00ddff.png'},
-                    '00ff11': { 'uri': 'icon/comet_16_00ff11.png'},
-                    'ff0000': { 'uri': 'icon/comet_16_ff0000.png'},
-                    'd00055': { 'uri': 'icon/comet_16_d00055.png'},
-                    'ff8000': { 'uri': 'icon/comet_16_ff8000.png'}
-                }
-            },
-            '32' : {
-                'black'    : '*/000000',
-                'white'    : '*/ffffff',
-                'antimony' : '*/00ddff',
-                'boron'    : '*/00ff11',
-                'lithium'  : '*/ff0000',
-                'potassium': '*/d00055',
-                'orange  ' : '*/ff8000',
-                '*': { '*' : 'ff8000',
-                    '000000': { 'uri': 'icon/comet_32_000000.png'},
-                    'ffffff': { 'uri': 'icon/comet_32_ffffff.png'},
-                    '00ddff': { 'uri': 'icon/comet_32_00ddff.png'},
-                    '00ff11': { 'uri': 'icon/comet_32_00ff11.png'},
-                    'ff0000': { 'uri': 'icon/comet_32_ff0000.png'},
-                    'd00055': { 'uri': 'icon/comet_32_d00055.png'},
-                    'ff8000': { 'uri': 'icon/comet_32_ff8000.png'}
-                }
-            },
-            '64' : {
-                'black'    : '*/000000',
-                'white'    : '*/ffffff',
-                'antimony' : '*/00ddff',
-                'boron'    : '*/00ff11',
-                'lithium'  : '*/ff0000',
-                'potassium': '*/d00055',
-                'orange  ' : '*/ff8000',
-                '*': { '*' : 'ff8000',
-                    '000000': { 'uri': 'icon/comet_64_000000.png'},
-                    'ffffff': { 'uri': 'icon/comet_64_ffffff.png'},
-                    '00ddff': { 'uri': 'icon/comet_64_00ddff.png'},
-                    '00ff11': { 'uri': 'icon/comet_64_00ff11.png'},
-                    'ff0000': { 'uri': 'icon/comet_64_ff0000.png'},
-                    'd00055': { 'uri': 'icon/comet_64_d00055.png'},
-                    'ff8000': { 'uri': 'icon/comet_64_ff8000.png'}
-                }
-            },
-            '128' : {
-                'black'    : '*/000000',
-                'white'    : '*/ffffff',
-                'antimony' : '*/00ddff',
-                'boron'    : '*/00ff11',
-                'lithium'  : '*/ff0000',
-                'potassium': '*/d00055',
-                'orange  ' : '*/ff8000',
-                '*': { '*' : 'ff8000',
-                    '000000': { 'uri': 'icon/comet_128_000000.png'},
-                    'ffffff': { 'uri': 'icon/comet_128_ffffff.png'},
-                    '00ddff': { 'uri': 'icon/comet_128_00ddff.png'},
-                    '00ff11': { 'uri': 'icon/comet_128_00ff11.png'},
-                    'ff0000': { 'uri': 'icon/comet_128_ff0000.png'},
-                    'd00055': { 'uri': 'icon/comet_128_d00055.png'},
-                    'ff8000': { 'uri': 'icon/comet_128_ff8000.png'}
-                }
-            }
+  "use strict";
+  /**
+   * Internal database of the known icons.
+   * Initially filled with the default icons.
+   */
+  return {
+    'CometVisu' : { '*' : '128',
+      '16' : {
+        'black'    : '*/000000',
+        'white'    : '*/ffffff',
+        'antimony' : '*/00ddff',
+        'boron'    : '*/00ff11',
+        'lithium'  : '*/ff0000',
+        'potassium': '*/d00055',
+        'orange  ' : '*/ff8000',
+        '*': { '*' : 'ff8000',
+          '000000': { 'uri': 'icon/comet_16_000000.png'},
+          'ffffff': { 'uri': 'icon/comet_16_ffffff.png'},
+          '00ddff': { 'uri': 'icon/comet_16_00ddff.png'},
+          '00ff11': { 'uri': 'icon/comet_16_00ff11.png'},
+          'ff0000': { 'uri': 'icon/comet_16_ff0000.png'},
+          'd00055': { 'uri': 'icon/comet_16_d00055.png'},
+          'ff8000': { 'uri': 'icon/comet_16_ff8000.png'}
         }
-    };
+      },
+      '32' : {
+        'black'    : '*/000000',
+        'white'    : '*/ffffff',
+        'antimony' : '*/00ddff',
+        'boron'    : '*/00ff11',
+        'lithium'  : '*/ff0000',
+        'potassium': '*/d00055',
+        'orange  ' : '*/ff8000',
+        '*': { '*' : 'ff8000',
+          '000000': { 'uri': 'icon/comet_32_000000.png'},
+          'ffffff': { 'uri': 'icon/comet_32_ffffff.png'},
+          '00ddff': { 'uri': 'icon/comet_32_00ddff.png'},
+          '00ff11': { 'uri': 'icon/comet_32_00ff11.png'},
+          'ff0000': { 'uri': 'icon/comet_32_ff0000.png'},
+          'd00055': { 'uri': 'icon/comet_32_d00055.png'},
+          'ff8000': { 'uri': 'icon/comet_32_ff8000.png'}
+        }
+      },
+      '64' : {
+        'black'    : '*/000000',
+        'white'    : '*/ffffff',
+        'antimony' : '*/00ddff',
+        'boron'    : '*/00ff11',
+        'lithium'  : '*/ff0000',
+        'potassium': '*/d00055',
+        'orange  ' : '*/ff8000',
+        '*': { '*' : 'ff8000',
+          '000000': { 'uri': 'icon/comet_64_000000.png'},
+          'ffffff': { 'uri': 'icon/comet_64_ffffff.png'},
+          '00ddff': { 'uri': 'icon/comet_64_00ddff.png'},
+          '00ff11': { 'uri': 'icon/comet_64_00ff11.png'},
+          'ff0000': { 'uri': 'icon/comet_64_ff0000.png'},
+          'd00055': { 'uri': 'icon/comet_64_d00055.png'},
+          'ff8000': { 'uri': 'icon/comet_64_ff8000.png'}
+        }
+      },
+      '128' : {
+        'black'    : '*/000000',
+        'white'    : '*/ffffff',
+        'antimony' : '*/00ddff',
+        'boron'    : '*/00ff11',
+        'lithium'  : '*/ff0000',
+        'potassium': '*/d00055',
+        'orange  ' : '*/ff8000',
+        '*': { '*' : 'ff8000',
+          '000000': { 'uri': 'icon/comet_128_000000.png'},
+          'ffffff': { 'uri': 'icon/comet_128_ffffff.png'},
+          '00ddff': { 'uri': 'icon/comet_128_00ddff.png'},
+          '00ff11': { 'uri': 'icon/comet_128_00ff11.png'},
+          'ff0000': { 'uri': 'icon/comet_128_ff0000.png'},
+          'd00055': { 'uri': 'icon/comet_128_d00055.png'},
+          'ff8000': { 'uri': 'icon/comet_128_ff8000.png'}
+        }
+      }
+    }
+  };
 });
