84ff82493f6ff8178d7cbe870c373afd7e57acce
Tobias Bräutigam <peuter@users.noreply.github.com>
2016-04-23 21:15:12 +0200

    rename source files to use CamelCase syntax

--- src/icon/iconconfig.js
+++ src/icon/iconconfig.js
@@ -15,7 +15,7 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
-define( ['lib/icontools'], function() {
+define( ['lib/IconTools'], function() {
   "use strict";
   /**
    * Internal database of the known icons.
