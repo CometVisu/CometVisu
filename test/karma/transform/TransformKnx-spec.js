5464ab57e23ccbbcf2e4a0572327cd007f73d2cb
ChristianMayer <mail@christianmayer.de>
2016-04-25 23:01:26 +0200

    Fix DPT:4 test case and clean up code

--- test/karma/transform/TransformKnx-spec.js
+++ test/karma/transform/TransformKnx-spec.js
@@ -22,13 +22,13 @@ define(['TransformDefault', 'TransformKnx'], function(Transform) {
     { transform: 'DPT:3',     type: 'decode', source: 0, target: 0    },
     
     { transform: 'DPT:4',     type: 'encode', source: 'a',  target: '8061', noNumber: true },
-    { transform: 'DPT:4',     type: 'decode', source: 0x61, target: 'a'    },
+    { transform: 'DPT:4',     type: 'decode', source: '61', target: 'a'    },
   ];
 
   describe('checking knx transforms', function() {
     // run testcases
-    testcases.forEach( function( testcase ){
-      it( 'should transform ' + testcase.transform + ' ' + testcase.type + ' "' + testcase.source + '"', function(){
+    testcases.forEach( function( testcase, index ){
+      it( 'should transform ' + testcase.transform + ' ' + testcase.type + ' "' + testcase.source + '" (test #' + index + ')', function(){
         switch( testcase.type ) {
           case 'encode':
             // test direct
@@ -50,96 +50,5 @@ define(['TransformDefault', 'TransformKnx'], function(Transform) {
         }
       });
     });
-
-    it('should transform DPT 1', function() {
-      expect(Transform.Transform['DPT:1'].encode(0)).toEqual('80');
-      expect(Transform.Transform['DPT:1'].encode('0')).toEqual('80');
-      expect(Transform.Transform['DPT:1'].encode(1)).toEqual('81');
-      expect(Transform.Transform['DPT:1'].encode('1')).toEqual('81');
-      expect(Transform.Transform['DPT:1'].decode(0)).toEqual(0);
-      expect(Transform.Transform['DPT:1'].decode(1)).toEqual(1);
-      expect(Transform.Transform['DPT:1.001'].encode(0)).toEqual('80');
-    });
-    /*
-    it('should transform switch values', function() {
-      expect(Transform.Transform['OH:switch'].encode(0)).toEqual('OFF');
-      expect(Transform.Transform['OH:switch'].encode(1)).toEqual('ON');
-      expect(Transform.Transform['OH:switch'].decode('ON')).toEqual(1);
-      expect(Transform.Transform['OH:switch'].decode('OFF')).toEqual(0);
-
-      expect(Transform.Transform['OH:switch'].decode('NaN')).toEqual(0);
-      expect(Transform.Transform['OH:switch'].decode('Uninitialized')).toEqual(0);
-    });
-
-    it('should transform contact values', function() {
-      expect(Transform.Transform['OH:contact'].encode(0)).toEqual('CLOSED');
-      expect(Transform.Transform['OH:contact'].encode(1)).toEqual('OPEN');
-      expect(Transform.Transform['OH:contact'].decode('OPEN')).toEqual(1);
-      expect(Transform.Transform['OH:contact'].decode('CLOSED')).toEqual(0);
-
-      expect(Transform.Transform['OH:contact'].decode('NaN')).toEqual(0);
-      expect(Transform.Transform['OH:contact'].decode('Uninitialized')).toEqual(0);
-    });
-
-    it('should transform rollershutter values', function() {
-      expect(Transform.Transform['OH:rollershutter'].encode(0)).toEqual('UP');
-      expect(Transform.Transform['OH:rollershutter'].encode(1)).toEqual('DOWN');
-      expect(Transform.Transform['OH:rollershutter'].decode('DOWN')).toEqual(1);
-      expect(Transform.Transform['OH:rollershutter'].decode('UP')).toEqual(0);
-    });
-
-    it('should transform dimmer values', function() {
-      expect(Transform.Transform['OH:dimmer'].encode("0")).toEqual(0);
-      expect(Transform.Transform['OH:dimmer'].encode("50")).toEqual(50);
-      expect(Transform.Transform['OH:dimmer'].decode('OFF')).toEqual(0);
-      expect(Transform.Transform['OH:dimmer'].decode('ON')).toEqual(100);
-      expect(Transform.Transform['OH:dimmer'].decode('53')).toEqual(53);
-      expect(Transform.Transform['OH:dimmer'].decode('NaN')).toEqual(0);
-      expect(Transform.Transform['OH:dimmer'].decode('Uninitialized')).toEqual(0);
-    });
-
-    it('should transform number values', function() {
-      expect(Transform.Transform['OH:number'].encode("0.5")).toEqual(0.5);
-      expect(Transform.Transform['OH:number'].encode("50")).toEqual(50);
-      expect(Transform.Transform['OH:number'].decode('0.5')).toEqual(0.5);
-      expect(Transform.Transform['OH:number'].decode('NaN')).toEqual(0);
-      expect(Transform.Transform['OH:number'].decode('Uninitialized')).toEqual(0);
-    });
-
-    it('should transform string values', function() {
-      expect(Transform.Transform['OH:string'].encode("example string")).toEqual("example string");
-      expect(Transform.Transform['OH:string'].decode("example string")).toEqual("example string");
-    });
-
-    it('should transform datetime values', function() {
-      var dateString = "2016-12-24T16:13:52Z";
-      var date = new Date(dateString);
-      expect(Transform.Transform['OH:datetime'].encode(date)).toEqual(date.toLocaleDateString());
-      expect(Transform.Transform['OH:datetime'].decode(dateString)).toEqual(date);
-
-      expect(Transform.Transform['OH:datetime'].decode('NaN')).toEqual("-");
-      expect(Transform.Transform['OH:datetime'].decode('Uninitialized')).toEqual("-");
-    });
-
-    it('should transform time values', function() {
-      var date = new Date("2016-12-24T16:13:52Z");
-      expect(Transform.Transform['OH:time'].encode(date)).toEqual(date.toLocaleTimeString());
-      date = new Date();
-      date.setHours(12);
-      date.setMinutes(53);
-      date.setSeconds(13);
-      expect(Transform.Transform['OH:time'].decode("12:53:13").toLocaleTimeString("de-DE")).toEqual(date.toLocaleTimeString("de-DE"));
-
-      expect(Transform.Transform['OH:time'].decode('NaN')).toEqual("-");
-      expect(Transform.Transform['OH:time'].decode('Uninitialized')).toEqual("-");
-    });
-
-    it('should transform color values', function() {
-      // encode RGB -> HSV
-      expect(Transform.Transform['OH:color'].encode([59, 60, 64])).toEqual([228, 7.8, 25.1]);
-      // decode HSV -> RGB
-      expect(Transform.Transform['OH:color'].decode("228, 7.8, 25.1")).toEqual([59, 60, 64]);
-    });
-    */
   });
 });
