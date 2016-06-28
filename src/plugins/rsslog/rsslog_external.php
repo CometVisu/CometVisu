9f589c7e20390fb03bbd2c2012fdc331cfe52692
Christian Tramnitz <ctr49@users.noreply.github.com>
2016-06-28 15:55:43 +0200

    Update rsslog_external.php
    as suggested in #219

--- src/plugins/rsslog/rsslog_external.php
+++ src/plugins/rsslog/rsslog_external.php
@@ -1,12 +1,12 @@
 <?php
 /*****************************************************************************/
 /* rsslog_external.php - A simple wrapper to fetch cross-domain content      */
-/* version 0.0.3                                                             */
+/* version 0.0.4                                                             */
 /* (c) 2014 by ctr http://knx-user-forum.de/members/ctr.html                 */
 /* Licenced under the GPLv3                                                  */
 /*****************************************************************************/
 
-$json_url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' . ($_GET["url"]) . '&output=json';
+$json_url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' . ($_GET["url"]) . '?' . time() . '&output=json';
 
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json; charset=utf-8');
