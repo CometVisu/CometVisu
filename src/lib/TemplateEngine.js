695d71e8ee4135deff36b3fb7c05c47dcff589ac
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-08-14 13:16:39 +0200

    update banner to remove @module and @title from it

--- src/lib/TemplateEngine.js
+++ src/lib/TemplateEngine.js
@@ -1,4 +1,4 @@
-/* templateengine.js 
+/* TemplateEngine.js 
  * 
  * copyright (c) 2010-2016, Christian Mayer and the CometVisu contributers.
  * 
@@ -15,9 +15,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
- *
- * @module TemplateEngine 
- * @title  CometVisu TemplateEngine 
  */
 
 
