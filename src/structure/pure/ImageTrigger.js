5e5e4d3b50fa1dd8947656d57d223cbc92680e2e
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-08-14 11:32:15 +0200

    colspan 0 example added

--- src/structure/pure/ImageTrigger.js
+++ src/structure/pure/ImageTrigger.js
@@ -53,6 +53,17 @@
  *   <address transform="DPT:16.001" mode="readwrite">0/0/0</address>
  * </imagetrigger>
  *
+ * @widget_example <meta>
+ *   <caption>Disable layout width by settings it to '0', to have widget with == image width</caption>
+ *   <screenshot name="image_trigger_colspan0">
+ *     <data address="0/0/0">1</data>
+ *   </screenshot>
+ * </meta>
+ * <imagetrigger src="icon/CometVisu_orange" suffix="png" sendValue="clicked" type="show" width="45px" height="32px">
+ *   <layout colspan="0" />
+ *   <address transform="DPT:1.001" mode="readwrite">0/0/0</address>
+ * </imagetrigger>
+ *
  * @module structure/pure/ImageTrigger
  * @requires structure/pure
  * @author Christian Mayer
