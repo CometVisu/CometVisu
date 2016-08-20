76c15a2b786a8a07319ddc7c6a5270c03fec5a5b
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-08-20 16:46:55 +0200

    another try

--- .doc/deploy.sh
+++ .doc/deploy.sh
@@ -56,12 +56,15 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+ls -la .
+ls -la ./settings
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in settings/keys/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../settings/keys/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
