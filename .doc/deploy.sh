384d0634fa62840454904bece1ef2355b8cb83ae
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-08-20 16:23:35 +0200

    and back to the deploy again

--- .doc/deploy.sh
+++ .doc/deploy.sh
@@ -4,6 +4,13 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="user-manual"
 TARGET_BRANCH="gh-pages"
 
+function createDocs {
+  sphinx-build -b html doc/manual/de/rst out
+  grunt jsdoc:html --targetDir=out
+  grunt screenshots --browserName=firefox
+  sphinx-build -b html doc/manual/de/rst out
+}
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy;"
@@ -26,6 +33,12 @@ git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our creation script
+createDocs
 
 # Now let's go have some fun with the cloned repo
 cd out
