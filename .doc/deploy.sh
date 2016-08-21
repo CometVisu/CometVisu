67a7024ecc370f37f8bc99942dc677fa835484b4
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-08-21 20:02:53 +0200

    use python3

--- .doc/deploy.sh
+++ .doc/deploy.sh
@@ -5,10 +5,10 @@ SOURCE_BRANCH="user-manual"
 TARGET_BRANCH="gh-pages"
 
 function createDocs {
-  sphinx-build -b html doc/manual/de/rst out/de/manual
+  python3 sphinx-build -b html doc/manual/de/rst out/de/manual
   grunt jsdoc:html --targetDir=out/api
   grunt screenshots --browserName=firefox
-  sphinx-build -b html doc/manual/de/rst out/de/manual
+  python3 sphinx-build -b html doc/manual/de/rst out/de/manual
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
