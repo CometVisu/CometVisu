6883a37df6d8d83e9bbd2c5a2e6a7ae454058ba1
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-10-30 00:05:29 +0200

    fix refresh

--- src/lib/cv/role/Refresh.js
+++ src/lib/cv/role/Refresh.js
@@ -17,8 +17,10 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
-define(['joose'], function() {
+define(['joose', 'lib/cv/utils/Timer'], function() {
   Role("cv.role.Refresh", {
+
+    requires: ['getDomElement'],
 
     has: {
       refresh : { is: 'r', init: 0 }
@@ -26,7 +28,7 @@ define(['joose'], function() {
 
     my: {
       after: {
-        parse: function( xml, path, flavour, widgetType ) {
+        parse: function( xml, path ) {
           var data = templateEngine.widgetDataGet(path);
           $e = $(xml);
           data.refresh = $e.attr('refresh') ? $e.attr('refresh')*1000 : 0;
@@ -36,9 +38,45 @@ define(['joose'], function() {
     after : {
       initialize : function (props) {
         cv.MessageBroker.my.subscribe("setup.dom.finished", function() {
-          templateEngine.setupRefreshAction( this.getPath(), this.getRefresh() );
+          this.setupRefreshAction();
         }, this);
       }
     },
+
+    methods: {
+
+      setupRefreshAction: function() {
+        if (this.getRefresh() && this.getRefresh() > 0) {
+          var
+            element = this.getDomElement(),
+            target = $('img', element)[0] || $('iframe', element)[0],
+            src = target.src;
+          if (src.indexOf('?') < 0)
+            src += '?';
+
+          cv.utils.Timer.start(this.getRefresh(), function() {
+            this.refreshAction(target, src);
+          }, this);
+        }
+      },
+
+      refreshAction: function(target, src) {
+        /*
+         * Special treatment for (external) iframes: we need to clear it and reload
+         * it in another thread as otherwise stays blank for some targets/sites and
+         * src = src doesnt work anyway on external This creates though some
+         * "flickering" so we avoid to use it on images, internal iframes and others
+         */
+        var parenthost = window.location.protocol + "//" + window.location.host;
+        if (target.nodeName == "IFRAME" && src.indexOf(parenthost) != 0) {
+          target.src = '';
+          cv.utils.Timer.once(function () {
+            target.src = src;
+          });
+        } else {
+          target.src = src + '&' + new Date().getTime();
+        }
+      }
+    }
   });
 });
