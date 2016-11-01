52a644d113b6284d5e9131e2a0fff34fa12f61c2
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-11-01 18:25:38 +0100

    fix switch tests

--- test/karma/structure/pure/Switch-spec.js
+++ test/karma/structure/pure/Switch-spec.js
@@ -5,20 +5,13 @@
  * @since 2016
  */
 
-define( ['TemplateEngine', '_common', 'CometVisuMockup', 'widget_switch'], function(engine, design, ClientMockup) {
+define( ['widget_switch'], function() {
 
   describe("testing a switch", function() {
-
-    var templateEngine = engine.getInstance();
-    templateEngine.visu = new ClientMockup();
-
     it("should test the switch creator", function() {
-      var creator = design.basicdesign.getCreator("switch");
 
-      var xml = document.createElement('template');
-      xml.innerHTML = '<switch><label>Test</label><address transform="DPT:1.001" mode="readwrite">12/7/37</address></switch>';
-      xml = xml.firstChild;
-      var switchWidget = $(creator.create(xml, 'id_0', 'potassium', 'switch'));
+      var res = this.createTestElement("switch", {flavour: 'potassium'}, '<label>Test</label>');
+      var switchWidget = $(res.getDomElement().find(".switch"));
 
       expect(switchWidget).toHaveFlavour('potassium');
       var actor = switchWidget.find(".actor");
@@ -34,83 +27,53 @@ define( ['TemplateEngine', '_common', 'CometVisuMockup', 'widget_switch'], funct
       expect(label).not.toBeNull();
       expect(label.text()).toBe("Test");
 
-      var data = templateEngine.widgetDataGet('id_0');
-      expect(data.on_value).toBe(1);
-      expect(data.off_value).toBe(0);
+      expect(res.getOnValue()).toBe(1);
+      expect(res.getOffValue()).toBe(0);
     });
 
     it("should test the switch creator with different on/off values", function() {
-      var creator = design.basicdesign.getCreator("switch");
+      var res = this.createTestElement("switch", {on_value: "turn_on", off_value: "turn_off"});
 
-      var xml = document.createElement('template');
-      xml.innerHTML = '<switch on_value="turn_on" off_value="turn_off"><label>Test</label><address transform="DPT:1.001" mode="readwrite">12/7/37</address></switch>';
-      xml = xml.firstChild;
-      creator.create(xml, 'id_0', 'potassium', 'switch');
-
-      var data = templateEngine.widgetDataGet('id_0');
-      expect(data.on_value).toBe('turn_on');
-      expect(data.off_value).toBe('turn_off');
+      expect(res.getOnValue()).toBe('turn_on');
+      expect(res.getOffValue()).toBe('turn_off');
     });
 
     it("should update a switch", function() {
-      var creator = design.basicdesign.getCreator("switch");
+      var res = this.createTestElement("switch", {}, '<label>Test</label>');
 
-      var xml = document.createElement('template');
-      xml.innerHTML = '<switch><label>Test</label><address transform="DPT:1.001" mode="readwrite">12/7/37</address></switch>';
-      xml = xml.firstChild;
-      var switchString = creator.create(xml, 'id_0', null, 'switch');
-      var container =document.createElement('div');
-      container.setAttribute("class","widget_container");
-      container.setAttribute("id", 'id_0');
-      container.innerHTML = switchString;
-      document.body.appendChild(container);
-
-      creator.update.call(container.children[0],'12/7/37', 1);
-      var actor = $(container.children[0].querySelectorAll('.actor')[0]);
+      res.update('12/7/37', 1);
+      var actor = $(this.container.children[0].querySelectorAll('.actor')[0]);
       expect(actor).not.toBe(null);
 
       expect(actor).toHaveClass("switchPressed");
       expect(actor).not.toHaveClass("switchUnpressed");
 
-      creator.update.call(container.children[0],'12/7/37', 0);
+      res.update('12/7/37', 0);
       expect(actor).toHaveClass("switchUnpressed");
       expect(actor).not.toHaveClass("switchPressed");
-
-      document.body.removeChild(container);
     });
 
     it('should trigger the switch action', function() {
       spyOn(templateEngine.visu, 'write');
 
-      var creator = design.basicdesign.getCreator("switch");
+      var res = this.createTestElement("switch", {}, '<label>Test</label>');
 
-      var xml = document.createElement('template');
-      xml.innerHTML = '<switch><label>Test</label><address transform="DPT:1.001" mode="readwrite">12/7/37</address></switch>';
-      xml = xml.firstChild;
-      var switchString = creator.create(xml, 'id_0', null, 'switch');
-      var container =document.createElement('div');
-      container.setAttribute("class","widget_container");
-      container.setAttribute("id", 'id_0');
-      container.innerHTML = switchString;
-      document.body.appendChild(container);
-      var actor = container.children[0].querySelectorAll('.actor')[0];
+      var actor = res.getActor();
       expect(actor).not.toBe(null);
 
       //canceled call
-      creator.action('id_0', actor, true);
+      res.action('id_0', actor, true);
       expect(templateEngine.visu.write).not.toHaveBeenCalled();
 
-      creator.update.call(container.children[0],'12/7/37', 0);
+      res.update('12/7/37', 0);
 
-      creator.action('id_0', actor, false);
+      res.action('id_0', actor, false);
       expect(templateEngine.visu.write).toHaveBeenCalledWith('12/7/37', '81');
 
-      creator.update.call(container.children[0],'12/7/37', 1);
+      res.update('12/7/37', 1);
 
-      creator.action('id_0', actor, false);
+      res.action('id_0', actor, false);
       expect(templateEngine.visu.write).toHaveBeenCalledWith('12/7/37', '80');
-      
-      document.body.removeChild(container);
     });
   });
 });
