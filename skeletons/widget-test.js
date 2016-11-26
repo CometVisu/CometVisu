0b91a08856f94e86d0d26599c37ed90e5e869fdd
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-11-26 15:59:18 +0100

    update test skeleton

--- skeletons/widget-test.js
+++ skeletons/widget-test.js
@@ -2,20 +2,15 @@
  * Unit tests for %WIDGET_NAME% widget
  *
  */
+describe("testing a %WIDGET_NAME% widget", function() {
 
-define( ['TemplateEngine', '_common', 'widget_%WIDGET_NAME%'], function(engine, design) {
+  it("should test the %WIDGET_NAME% creator", function() {
+    var res = this.createTestWidgetString("%WIDGET_NAME%", {}, "<label>Test</label>");
+    var widget = qx.bom.Html.clean([res[1]])[0];
+    var obj = res[0];
 
-  describe("testing a %WIDGET_NAME% widget", function() {
-    var templateEngine = engine.getInstance();
-
-    it("should test the %WIDGET_NAME% creator", function() {
-      var widget = $(this.createTestWidgetString("%WIDGET_NAME%", {}, "<label>Test</label>")[1]);
-    
-      expect(widget).toHaveClass('%WIDGET_NAME%');
-      expect(widget.find("div.label").text()).toBe('Test');
-
-      var data = templateEngine.widgetDataGet('id_0');
-      expect(data.path).toBe("id_0");
-    });
+    expect(widget).toHaveClass('%WIDGET_NAME%');
+    expect(widget).toHaveLabel('Test');
+    expect(obj.getPath()).toBe("id_0");
   });
 });
