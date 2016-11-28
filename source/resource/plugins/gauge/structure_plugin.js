66f7445261ab897d3aad238b2930d8ef16e3e0c9
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-11-28 18:38:22 +0100

    delete old banner

--- source/resource/plugins/gauge/structure_plugin.js
+++ source/resource/plugins/gauge/structure_plugin.js
@@ -1,21 +1,4 @@
-﻿/* structure_custom.js (c) 01/2014 by NetFritz [NetFritz at gmx dot de]
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
- */
- 
-/**
+﻿/**
  * This plugins integrates Gauge (in canvas) into the visualization
  *
  * @author NetFritz [NetFritz at gmx dot de]
