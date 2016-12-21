55f1ce94567cfd572f1c7259e195a36b1f767c25
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-12-21 22:31:31 +0100

    add docs

--- source/class/cv/Transform.js
+++ source/class/cv/Transform.js
@@ -19,9 +19,26 @@
 
 
 /**
- * This class defines the default transforms:
- *   encode: transform JavaScript to bus value
- *   decode: transform bus to JavaScript value
+ * This class defines some default transformations like <code>raw</code>, <code>int</code> and <code>float</code>.
+ * New transformation can be added and used. For example an openHAB transformation for a switch item is added by
+ * {@link cv.transforms.OpenHab} like this:
+ *
+ * <pre class="javascript">
+ *   cv.Transform.addTransform('OH', {
+ *    'switch': {
+ *      name: 'OH_Switch',
+ *       encode: function (phy) {
+ *         return phy == 1 ? 'ON' : 'OFF';
+ *      },
+ *
+ *       decode: function (string) {
+ *         if (cv.transforms.OpenHab.isUndefined(string)) return 0;
+ *         return (string == "ON" || parseInt(string) > 0) ? 1 : 0;
+ *       }
+ *     }
+ *   });
+ * </pre>
+ *
  * @author Christian Mayer
  * @since 2010
  */
@@ -64,11 +81,16 @@ qx.Class.define('cv.Transform', {
       }
     },
 
-    /****************************************************************************
+    /* ***************************************************************************
      * All functions below are only in this, i.e. the default, file.
      * All further transforms will only have the above data structure.
-     ***************************************************************************/
+     ************************************************************************** */
 
+    /**
+     * Add transformation rules to the registry
+     * @param prefix {String} Tranformation prefix (e.g. DPT for KNX tranformations or OH for openHAB transformations)
+     * @param transforms {Map} map of transformations
+     */
     addTransform: function (prefix, transforms) {
       for (var trans in transforms) {
         if (transforms[trans].link) {
@@ -82,12 +104,23 @@ qx.Class.define('cv.Transform', {
     /**
      * Enforce that value stays within range
      * When value is not a valid number, the min value is returned
+     * @param min {Number} lower threshold
+     * @param value {var} value to clip
+     * @param max {Number} upper threshold
+     * @return {Number} the clipped value
      */
     clip: function (min, value, max) {
       value = +value; // enforce number
       return value > min ? (value > max ? max : value) : min;
     },
 
+    /**
+     * transform JavaScript to bus value
+     *
+     * @param transformation {String} type of the transformation
+     * @param value {var} value to transform
+     * @return {var} the encoded value
+     */
     encode: function (transformation, value) {
       if (cv.Config.testMode === true) {
         return value;
@@ -98,6 +131,12 @@ qx.Class.define('cv.Transform', {
         .encode(value) : value);
     },
 
+    /**
+     * transform bus to JavaScript value
+     * @param transformation {String} type of the transformation
+     * @param value {var} value to transform
+     * @return {var} the decoded value
+     */
     decode: function (transformation, value) {
       if (cv.Config.testMode === true) {
         return value;
