7d268a138febb0a84324d8ef78cc845270bc000a
Tobias Bräutigam <tbraeutigam@gmail.com>
2016-12-29 12:09:38 +0100

    fix json test

--- source/test/karma/io/parser/Json-spec.js
+++ source/test/karma/io/parser/Json-spec.js
@@ -8,7 +8,7 @@
 describe('test the json fallback parser', function () {
 
   it('should parse wrong json data', function() {
-    var res = cv.io.parser.Json.parse("{0: true}{1: true}");
+    var res = cv.io.parser.Json.parse('{"0": true}{"1": true}');
     expect(res["1"]).toBeTruthy();
   })
 });
