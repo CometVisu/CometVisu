9d9b300ce02985b89de971c4f8caf5977d95b4b3
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-02-20 17:04:32 +0100

    blind fixing the 2d-demo

--- source/resource/demo/media/demo_2d_backdrop_red_pot.js
+++ source/resource/demo/media/demo_2d_backdrop_red_pot.js
@@ -1,37 +1,25 @@
-require.config({
-  baseUrl: './',
-  waitSeconds: 30, // default: 7 seconds
-  paths: {
-    'jquery':            '../../dependencies/jquery',
-    'CometvisuClient':  '../../lib/CometVisuClient',
-    'TransformDefault': '../../transforms/TransformDefault',
-    'TransformKnx':     '../../transforms/TransformKnx'
-  }
-});
+// import namespaces from parent window
+var qx = window.parent.qx;
+var cv = window.parent.cv;
 
-require([
-  'jquery', 'cometvisu-client', 'TransformDefault', 'TransformKnx'
-], function( jq, CometVisu ) {
-  "use strict";
-  
-  var 
-    thisGA = '12/7/52',
-    thisTransform = 'DPT:5.001',
-    visu = new CometVisu('cgi-bin');
+var
+  thisGA = '12/7/52',
+  thisTransform = 'DPT:5.001',
+  visu = cv.Config.testMode ? new cv.io.Mockup() : new cv.io.Client('default');
 
-  visu.update = function( json ) // overload the handler
-  {
-    var h = Transform[thisTransform].decode( json[thisGA] );
-    var filling = $('#rect3855')[0];
-    filling.y.baseVal.value=200.57388 + (100-h)*2;
-    filling.height.baseVal.value = h*2;
-    $('#path3029-4')[0].setAttribute('d', 'm 524.85653,'+(200.57388+ (100-h)*2)+' a 100,37.795274 0 0 1 -200,0 100,37.795274 0 1 1 200,0 z')
-  }
-  
-  $(window).unload(function() {
-    visu.stop();
-  });
-  
-  visu.user = 'demo_user'; // example for setting a user
-  visu.subscribe( [thisGA] );
+visu.update = function( json ) // overload the handler
+{
+  var h = cv.Transform[thisTransform].decode( json[thisGA] );
+  var filling = qx.bom.Selector.query('#rect3855')[0];
+  filling.y.baseVal.value=200.57388 + (100-h)*2;
+  filling.height.baseVal.value = h*2;
+  qx.bom.element.Attribute.set(qx.bom.Selector.query('#path3029-4')[0], 'd', 'm 524.85653,'+(200.57388+ (100-h)*2)+' a 100,37.795274 0 0 1 -200,0 100,37.795274 0 1 1 200,0 z');
+};
+
+qx.event.Registration.addListener(window, "unload", function() {
+  visu.stop();
 });
+  
+visu.user = 'demo_user'; // example for setting a user
+visu.subscribe( [thisGA] );
+visu.login();
