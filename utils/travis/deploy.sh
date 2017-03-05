9e439d518efdf017e102a4e70767188ce667d555
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-03-05 17:49:28 +0100

    disable diff check for master

--- utils/travis/deploy.sh
+++ utils/travis/deploy.sh
@@ -61,11 +61,14 @@ cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-echo "checking diff"
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
+# as the changesets on master are too big we skip this check to prevent timeouts
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    echo "checking diff"
+    if [ -z `git diff --exit-code` ]; then
+        echo "No changes to the output on this push; exiting."
+        exit 0
+    fi
 fi
 
 # Commit the "changes", i.e. the new version.
