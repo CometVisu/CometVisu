6bf6e740c624da6bdf81f7caeb3f9c300077c10a
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-03-05 17:13:06 +0100

    disable diff check temporarily as the changeset is too big

--- utils/travis/deploy.sh
+++ utils/travis/deploy.sh
@@ -63,10 +63,10 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 echo "checking diff"
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+#if [ -z `git diff --exit-code` ]; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
