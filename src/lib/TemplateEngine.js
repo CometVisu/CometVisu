f15f914ead8c2f6277359024c144f102b89a1cc8
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-03-30 10:35:30 +0200

    handle %20 spaces in page name

--- src/lib/TemplateEngine.js
+++ src/lib/TemplateEngine.js
@@ -1118,8 +1118,11 @@ define([
               rememberLastPage = false;
             }
           }
+          // check if startpage contains a page name and find the page id
+          startpage = thisTemplateEngine.getPageIdByPath(startpage) || startpage;
+
           // check that startpage does exits
-          if( $('#'+startpage+'.page').length === 0 && thisTemplateEngine.getPageIdByPath(startpage) === null)
+          if($('#'+startpage+'.page').length === 0)
             startpage = 'id_';   // default to top most page
         }
         thisTemplateEngine.currentPage = $('#'+startpage);
@@ -1311,7 +1314,7 @@ define([
         // decode html code (e.g. like &apos; => ')
         page_name = $("<textarea/>").html(page_name).val();
         // remove escaped slashes
-        page_name = page_name.replace("\\\/","/");
+        page_name = decodeURI(page_name.replace("\\\/","/"));
 
         //      console.log("Page: "+page_name+", Scope: "+scope);
         var selector = (scope!=undefined && scope!=null) ? '.page[id^="'+scope+'"] h1:contains(' + page_name + ')' :  '.page h1:contains(' + page_name + ')';
