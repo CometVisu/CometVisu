eb2d69a71d13c5991b9050e0ebe4e858194a3345
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-10-16 19:02:27 +0200

    restore original

--- utils/travis/deploy.sh
+++ utils/travis/deploy.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="develop"
 TARGET_BRANCH="gh-pages"
-REPO_SLUG="cometvisu/CometVisu"
+REPO_SLUG="CometVisu/CometVisu"
 
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     echo "Skipping deploy in cron build"
@@ -11,15 +11,15 @@ if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
 fi
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-#if [ "$TRAVIS_PULL_REQUEST" != "false" ] || ( [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ] && [ "$TRAVIS_BRANCH" != "master" ] ); then
-#    echo "Skipping deploy;"
-#    exit 0
-#fi
-#
-#if [ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]; then
-#    echo "Not in main repository => skipping deploy;"
-#    exit 0
-#fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || ( [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ] && [ "$TRAVIS_BRANCH" != "master" ] ); then
+    echo "Skipping deploy;"
+    exit 0
+fi
+
+if [ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]; then
+    echo "Not in main repository => skipping deploy;"
+    exit 0
+fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "ATTENTION! Deploying docs from non master branch. Please change this!!!"
@@ -77,36 +77,36 @@ echo "generating feature yml file for homepage"
 echo "generating sitemap.xml for documentation"
 ./cv sitemap
 
-#echo "starting deployment..."
-## Now let's go have some fun with the cloned repo
-#cd out
-#git config user.name "Travis CI"
-#git config user.email "$COMMIT_AUTHOR_EMAIL"
-#
-## If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-## as the changesets on master are too big we skip this check to prevent timeouts
-#if [ "$TRAVIS_BRANCH" != "master" ]; then
-#    echo "checking diff"
-#    if [ -z `git diff --exit-code` ]; then
-#        echo "No changes to the output on this push; exiting."
-#        exit 0
-#    fi
-#fi
-#
-## Commit the "changes", i.e. the new version.
-## The delta will show diffs between new and old versions.
-#git add --all .
-#git commit -m "Deploy to GitHub Pages: ${SHA}"
-#
-## Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../utils/travis/deploy_key.enc -out deploy_key -d
-#chmod 600 deploy_key
-#eval `ssh-agent -s`
-#ssh-add deploy_key
-#
-## Now that we're all set up, we can push.
-#git push $SSH_REPO $TARGET_BRANCH
+echo "starting deployment..."
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# as the changesets on master are too big we skip this check to prevent timeouts
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    echo "checking diff"
+    if [ -z `git diff --exit-code` ]; then
+        echo "No changes to the output on this push; exiting."
+        exit 0
+    fi
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add --all .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../utils/travis/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
