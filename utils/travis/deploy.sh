ac2c3b7e7f90c4c403c088a4f92df62bee106c07
Tobias Bräutigam <tbraeutigam@gmail.com>
2017-10-16 18:10:47 +0200

    try to use the docker container for screenshot generation

--- utils/travis/deploy.sh
+++ utils/travis/deploy.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="develop"
 TARGET_BRANCH="gh-pages"
-REPO_SLUG="CometVisu/CometVisu"
+REPO_SLUG="peuter/CometVisu"
 
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     echo "Skipping deploy in cron build"
@@ -60,12 +60,12 @@ echo "updating english manual from source code doc comments"
 ./cv doc --process-versions
 
 echo "generating english manual, including screenshot generation for all languages"
-./cv doc --doc-type manual -c -f -l en -t build
+./docker-run ./cv doc --doc-type manual -c -f -l en -t build
 echo "generating german manual again with existing screenshots"
 ./cv doc --doc-type manual -f -l de
 
 echo "generate API screenshots"
-grunt screenshots --subDir=source --browserName=chrome --target=build --force
+./docker-run grunt screenshots --subDir=source --browserName=chrome --target=build --force
 
 # move the apiviewer to the correct version subfolder, including screenshots
 rm -r out/en/$VERSION/api
@@ -77,36 +77,36 @@ echo "generating feature yml file for homepage"
 echo "generating sitemap.xml for documentation"
 ./cv sitemap
 
-echo "starting deployment..."
-# Now let's go have some fun with the cloned repo
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-# as the changesets on master are too big we skip this check to prevent timeouts
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-    echo "checking diff"
-    if [ -z `git diff --exit-code` ]; then
-        echo "No changes to the output on this push; exiting."
-        exit 0
-    fi
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add --all .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../utils/travis/deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+#echo "starting deployment..."
+## Now let's go have some fun with the cloned repo
+#cd out
+#git config user.name "Travis CI"
+#git config user.email "$COMMIT_AUTHOR_EMAIL"
+#
+## If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+## as the changesets on master are too big we skip this check to prevent timeouts
+#if [ "$TRAVIS_BRANCH" != "master" ]; then
+#    echo "checking diff"
+#    if [ -z `git diff --exit-code` ]; then
+#        echo "No changes to the output on this push; exiting."
+#        exit 0
+#    fi
+#fi
+#
+## Commit the "changes", i.e. the new version.
+## The delta will show diffs between new and old versions.
+#git add --all .
+#git commit -m "Deploy to GitHub Pages: ${SHA}"
+#
+## Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../utils/travis/deploy_key.enc -out deploy_key -d
+#chmod 600 deploy_key
+#eval `ssh-agent -s`
+#ssh-add deploy_key
+#
+## Now that we're all set up, we can push.
+#git push $SSH_REPO $TARGET_BRANCH
