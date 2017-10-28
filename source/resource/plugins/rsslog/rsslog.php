9580b7b3d62a0c3b3ba306383316b2826ad9f218
Christian Mayer <ChristianMayer@users.noreply.github.com>
2017-10-28 23:29:03 +0200

    Update rsslog (#676)
    * Switch to PDO from outdated sqlite interface
    
    * Dynamically switch between sqlite3 and sqlite2
    
    * Fixes + remove support for DB version 1
    
    * Auto-Migrate sqlite2 to sqlite3
    
    * Add most of the info screen
    
    * Make output look more like having a CI
    
    * Implement update function
    
    * Added feedback
    
    * Acitvate live DB again for fix release

--- source/resource/plugins/rsslog/rsslog.php
+++ source/resource/plugins/rsslog/rsslog.php
@@ -33,6 +33,8 @@
 // 6. Update state:
 //    URL parameter "u" id of row
 //    URL parameter "state": new state
+// 7. Info page:
+//    URL parameter "info" Show an info page
 
 
 // look where to store DB
@@ -49,14 +51,18 @@ if (! is_writable($dbfile_dir))
     die ("Database $dbfile not writeable! make sure the file AND " .
         "the directory are writeable by the webserver!"); 
 
-// create database connection
-$db = sqlite_open($dbfile, 0666, $error);
-if (!$db) die ($error);
+$dbh = openDb( $dbfile );
 
 // create table if it doesn't exists
-create( $db );
+create( $dbh );
 
-if( isset($_GET['c']) )
+if( isset($_GET['info']) )
+{
+  showInfo( $dbh );
+} else if( isset($_GET['update']) )
+{
+  runUpdate( $dbh );
+} else if( isset($_GET['c']) )
 { 
   // store a new log
   $store = true;
@@ -71,19 +77,20 @@ if( isset($_GET['c']) )
   $log_tags    = $_GET['t'] ? $_GET['t'] : array();
   if(! is_array($log_tags))
     die("wrong format - use one or more t[]= for tags");
-  insert( $db, $log_content, $log_title, $log_tags, $log_mapping, $log_state );
+  insert( $dbh, $log_content, $log_title, $log_tags, $log_mapping, $log_state );
 } else if( isset($_GET['dump']) )
 {
-  $result = retrieve( $db, $log_filter, NULL, NULL );
+  $result = retrieve( $dbh, $log_filter, NULL, NULL, true );
   ?>
-<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" /></head><body>
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>RSSLog Information</title>
+<link type="text/css" rel="stylesheet" href="../../cometvisu_management.css"></head><body>
 <table border="1">
   <?php
   $records = 0;
   echo '<tr><th>ID</th><th>DateTime</th><th>Timestamp</th><th>Title</th><th>Content</th><th>Tags</th><th>Mapping</th><th>State</th></tr>';
-  while( sqlite_has_more($result) )
+  foreach ( $result as $row )
   {
-    $row = sqlite_fetch_array($result, SQLITE_ASSOC ); 
     echo '<tr>';
     echo '<td>' . $row['id'] . '</td>';
     echo '<td>' . date( DATE_ATOM, $row['t'] ) . '</td>';
@@ -105,7 +112,10 @@ if( isset($_GET['c']) )
 {
   $timestamp  = $_GET['r'] ? $_GET['r'] : '';
   $filter  = $_GET['f'] ? $_GET['f'] : '';
-  delete( $db, $timestamp, $filter );
+  delete( $dbh, $timestamp, $filter );
+?>
+Successfully run deletion.
+<?php
 } else if( isset($_GET['j']) )
 {
   header('Content-Type: application/json');
@@ -128,11 +138,10 @@ if( isset($_GET['c']) )
   $future = $_GET['future'];
 
   // retrieve data
-  $result = retrieve( $db, $log_filter, $state, $future );
+  $result = retrieve( $dbh, $log_filter, $state, $future );
   $first = true;
-  while( sqlite_has_more($result) )
+  while( $row = $result->fetch(PDO::FETCH_ASSOC) )
   {
-    $row = sqlite_fetch_array($result, SQLITE_ASSOC ); 
     if( !$first ) echo ",\n";
     echo '{';
     echo '"id": "' . $row['id'] . '",';
@@ -160,7 +169,7 @@ if( isset($_GET['c']) )
   $newstate = $_GET['state'];
   if (!is_numeric($newstate))
     die("wrong format - state is required and has to be numeric");
-  updatestate( $db, $id, $newstate );
+  updatestate( $dbh, $id, $newstate );
 ?>
 Successfully updated ID=<?php echo $id; ?>.
 <?php
@@ -168,7 +177,7 @@ Successfully updated ID=<?php echo $id; ?>.
   $id = $_GET['d'];
   if (!is_numeric($id))
     die("wrong format - id has to be numeric");
-  deleteentry( $db, $id );
+  deleteentry( $dbh, $id );
 ?>
 Successfully deleted ID=<?php echo $id; ?>.
 <?php
@@ -179,7 +188,7 @@ Successfully deleted ID=<?php echo $id; ?>.
   $future = $_GET['future'];
 
   // retrieve data
-  $result = retrieve( $db, $log_filter, $state, $future );
+  $result = retrieve( $dbh, $log_filter, $state, $future );
   echo '<?xml version="1.0"?>';
   ?>
 <rss version="2.0">
@@ -189,9 +198,8 @@ Successfully deleted ID=<?php echo $id; ?>.
     <description>RSS supplied logs</description>
   <?php
   // echo '<description>foo</description>';
-  while( sqlite_has_more($result) )
+  foreach ( $result as $row )
   {
-    $row = sqlite_fetch_array($result, SQLITE_ASSOC );
     $tags = ' [ id=' . $row['id']. ',state=' . $row['state'];
     if ($row['tags'])
         $tags .= ',' . $row['tags'];
@@ -208,43 +216,90 @@ Successfully deleted ID=<?php echo $id; ?>.
   <?php
 }
 
-sqlite_close($db);
+///////////////////////////////////////////////////////////////////////////////
+// Open database
+function openDb( $dbfile )
+{
+  ////////////////////////////////////////////////////////////////////////////
+  // Handle history:
+  // rsslog.php used to use sqlite2 which is outdated and replaced by sqlite3.
+  // As it is not clear whether the database file is already a sqlite3 we 
+  // assume it optimistically but check it later on when a problem occurs:
+  global $usedDBdriver;
+
+  // create database connection - assuming it's sqlite3
+  $dbh = new PDO('sqlite:' . $dbfile) or die("cannot open the database with PDO(sqlite)");
+  $usedDBdriver = 'sqlite';
+  
+  // check whether we can read
+  $q = "SELECT name FROM sqlite_master WHERE type='table'";
+  $result = $dbh->query( $q );
+  if (!$result) 
+  {
+    // open and read did not work => file might be sqlite2
+    if( in_array('sqlite2', PDO::getAvailableDrivers()) )
+    {
+      $dbh = new PDO('sqlite2:' . $dbfile) or die("cannot open the database with PDO(sqlite2)");
+      
+      // check whether we can read now
+      $q = "SELECT name FROM sqlite_master WHERE type='table'";
+      $result = $dbh->query( $q );
+      if (!$result) die("Database read with PDO(sqlite2) failed!");
+      
+      $usedDBdriver = 'sqlite2';
+    } 
+    else 
+      die("Database couldn't be open. Sqlite2 check couldn't be performed as driver is missing.");
+  }
+  
+  return $dbh;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // create tables if they don't exist
-function create( $db )
+function create( $dbh )
 {
+  // Database versions:
+  // 0: nothing
+  // 1: old version prior 2016
+  // 2: currently the latest one
   $logschema = 0;  // default: nothing exists yet
   
   // check: do we have a version information?
   $q = "SELECT name FROM sqlite_master WHERE type='table' AND name='Version';";
-  $result = sqlite_query( $db, $q, SQLITE_NUM );
+  $result = $dbh->query( $q );
   if (!$result) die("Cannot execute query. $q");
-  if( !sqlite_has_more($result) )
+  if( !$result->fetch(PDO::FETCH_NUM) )
   {
     // no table found - create it
     $q = 'CREATE TABLE Version(' .
         '  logschema INT' .
-        ');' . 
-        'INSERT INTO Version( logschema ) VALUES( 0 );';
-    $ok = sqlite_exec($db, $q, $error);
-    
-    if (!$ok)
-      die("Cannot execute query $q. $error");
+        ');';
+    $ok = $dbh->exec( $q );
+    if ($ok===false)
+      die("Cannot execute query. $q. " . end($dbh->errorInfo()));
     
     $q = "SELECT name FROM sqlite_master WHERE type='table' AND name='Logs';";
-    $result = sqlite_query( $db, $q, SQLITE_NUM );
+    $result = $dbh->query( $q );
     if (!$result) die("Cannot execute query. $q");
-    if( sqlite_has_more($result) )
+    if( $result->fetch(PDO::FETCH_NUM) )
     {
       // no Version table but Log table
       // => database version prior 2016
       $logschema = 1;
     }
+    
+    $q = "INSERT INTO Version( logschema ) VALUES( $logschema )";
+    $ok = $dbh->exec( $q );
+    if ($ok===false)
+      die("Cannot execute query. $q. " . end($dbh->errorInfo()));
   } else {
-    $q = "SELECT logschema FROM Version";
-    $res = sqlite_query( $db, $q, SQLITE_ASSOC );
-    $logschema = sqlite_fetch_single( $res );
+    $logschema = dbSchemaVersion( $dbh );
+    if( -1 === $logschema )
+    {
+      // this shouldn't happen - the table Version does exist but is empty
+      $logschemaNew = 2;
+    }
   }
   
   $currentSchema = 
@@ -263,64 +318,68 @@ function create( $db )
     case 0:  // inital setup of database
       // no table found - create it
       $q = 'CREATE TABLE Logs' . $currentSchema;
-      $ok = sqlite_exec($db, $q, $error);
-      if (!$ok) die("Cannot execute query $q. $error");
+      $ok = $dbh->exec( $q );
+      if ($ok===false) die("Cannot execute query $q. " . end($dbh->errorInfo()));
         
-      $logschema = 2;
+      $logschemaNew = 2;
       break;
       
     case 1:  // version without mapping
-      // note: SQLite2 has no ALTER TABLE - so do it the hard way
-      $q = 'CREATE TEMPORARY TABLE TempLogs' . $currentSchema .
-        'INSERT INTO TempLogs SELECT id, title, content, tags, "", t, state FROM Logs;' .
-        'DROP TABLE Logs;' .
-        'CREATE TABLE Logs' . $currentSchema .
-        'INSERT INTO Logs SELECT id, title, content, tags, mapping, t, state FROM TempLogs;' .
-        'DROP TABLE TempLogs;';
-      $ok = sqlite_exec($db, $q, $error);
-      if (!$ok) die("Cannot execute query $q. $error");
-    
-      $logschema = 2;
+      // note: SQLite2 has no ALTER TABLE - so do it the hard 
+      global $dbfile;
+      die("Error: Database version is too old! Please use a rsslog.php of the CometVisu release 0.9.x or 0.10.x to convert it or discard it by deleting the file '$dbfile'.");
+      
+      $logschemaNew = 2;
       break;
       
     case 2:  // current
+      $logschemaNew = 2;
       return; 
   }
   
   // bump logschema
-  $q = 'UPDATE Version SET logschema=' . $logschema . ';';
-  $ok = sqlite_exec($db, $q, $error);
-  if (!$ok) die("Cannot execute query. $error");
+  if( $logschema != $logschemaNew )
+  {
+    $q = 'UPDATE Version SET logschema=' . $logschemaNew . ';';
+    $ok = $dbh->exec( $q );
+    if ($ok===false) die("Cannot execute query $q. " . end($dbh->errorInfo()));
+  }
 }
 
 // insert a new log line
-function insert( $db, $content, $title, $tags, $mapping, $state )
+function insert( $dbh, $content, $title, $tags, $mapping, $state, $time = "datetime('now')" )
 {
   // store a new log line
   $q = 'INSERT INTO Logs(content, title, tags, mapping, t, state) VALUES( ' .
-       "  '" . sqlite_escape_string( $content ) . "'," .
-       "  '" . sqlite_escape_string( $title ) . "'," .
-       "  '" . sqlite_escape_string( implode(",",$tags) ) . "'," .
-       "  '" . sqlite_escape_string( $mapping ) . "'," .
-       "  datetime('now')," .
-       "  $state" .
+       "  ?," .
+       "  ?," .
+       "  ?," .
+       "  ?," .
+       "  $time," .
+       "  ?" .
        ')';
-  
-  $ok = sqlite_exec($db, $q, $error);
+  $sth = $dbh->prepare( $q );
+  $ok = $sth->execute( array($content, $title, implode(",",$tags), $mapping, $state) );
   
   if (!$ok)
-    die("Cannot execute query. $error (Title: $itle Content: $content Tags: $tags State: $state)");
+    die("Cannot execute query. " . end($dbh->errorInfo()) . " (Title: $itle Content: $content Tags: $tags State: $state)");
 }
 
 // return a handle to all the data
-function retrieve( $db, $filter, $state, $future )
+function retrieve( $dbh, $filter, $state, $future, $dump = false, $order = 'DESC', $asUnix = true )
 {
-  $filters = explode(',', $filter); // accept filters by separated by ,
-  foreach ($filters as $i => $val) {
-    $filters[$i] = " (tags LIKE '%" . sqlite_escape_string($val) . "%') ";
+  if( $filter === '' )
+  {
+    $filters = Array();
+    $filterString = '1=1';
+  } else {
+    $filters = explode(',', $filter); // accept filters by separated by ,
+    function substrmatch($s) { return "%$s%"; };
+    $filters = array_map( substrmatch, $filters );
+    $filterString = '(tags LIKE ?) ' . str_repeat( 'OR (tags LIKE ?) ', count( $filters )-1 );
   }
   
-  $q = "SELECT id, title, content, tags, mapping, state, strftime('%s', t) AS t FROM Logs WHERE (" . implode('OR', $filters) . ")";
+  $q = "SELECT id, title, content, tags, mapping, state, " . ($asUnix ? "strftime('%s', t) AS t" : 't') . " FROM Logs WHERE ($filterString) ";
   
   if (isset($state) AND is_numeric($state))
     $q .= " AND state=" . $state . " ";
@@ -330,45 +389,181 @@ function retrieve( $db, $filter, $state, $future )
   else
     $q .= " AND ((t  <= datetime('now') ))";
   
-  $q .= "ORDER by t DESC";
-  if (!isset($_GET['dump']))
+  $q .= "ORDER by t $order";
+  if (!$dump)
     if( !isset($_GET['limit']) || !is_numeric($_GET['limit']) || '0' == $_GET['limit'] )
       $q .= " LIMIT 100";
     else
       $q .= " LIMIT " . $_GET['limit'];
-  return sqlite_query( $db, $q, SQLITE_ASSOC );
+  
+  $sth = $dbh->prepare( $q );
+  $sth->execute( $filters );
+  return $sth;
 }
 
 // delete all log lines older than the timestamp and optional a filter
-function delete( $db, $timestamp, $filter )
+function delete( $dbh, $timestamp, $filter )
 {
-  $filters = explode(',', $filter); // accept filters by separated by ,
-  foreach ($filters as $i => $val) {
-    $filters[$i] = " (tags LIKE '%" . sqlite_escape_string($val) . "%') ";
+  if( $filter === '' )
+  {
+    $filters = Array();
+    $filterString = '1=1';
+  } else {
+    $filters = explode(',', $filter); // accept filters by separated by ,
+    function substrmatch($s) { return "%$s%"; };
+    $filters = array_map( substrmatch, $filters );
+    $filterString = '(tags LIKE ?) ' . str_repeat( 'OR (tags LIKE ?) ', count( $filters )-1 );
   }
 
-  $q = "DELETE from Logs WHERE t < datetime($timestamp, 'unixepoch') AND (" . implode('OR', $filters) . ")";
-  $ok = sqlite_exec($db, $q, $error);
+  $q = "DELETE from Logs WHERE t < datetime($timestamp, 'unixepoch') AND ($filterString)";
+  $sth = $dbh->prepare( $q );
+  $ok = $sth->execute( $filters );
   
   if (!$ok)
-    die("Cannot execute query. $error");
+    die("Cannot execute query. " . end($dbh->errorInfo()));
 }
 
-function deleteentry( $db, $id )
+function deleteentry( $dbh, $id )
 {
-  $q = "DELETE from Logs WHERE id = $id";
-  $ok = sqlite_exec($db, $q, $error);
+  $q = "DELETE from Logs WHERE id = ?";
+  $sth = $dbh->prepare( $q );
+  $ok = $sth->execute( array($id) );
   
   if (!$ok)
-    die("Cannot execute query. $error");
+    die("Cannot execute query. " . end($dbh->errorInfo()));
 }
 
-function updatestate( $db, $id, $newstate)
+function updatestate( $dbh, $id, $newstate)
 {
-  $q = 'UPDATE Logs SET state=' . $newstate . ' WHERE id=' . $id . ';';
-  $ok = sqlite_exec($db, $q, $error);
+  $q = 'UPDATE Logs SET state=? WHERE id=?';
+  $sth = $dbh->prepare( $q );
+  $ok = $sth->execute( array($newstate, $id) );
   
   if (!$ok)
-    die("Cannot execute query. $error");
+    die("Cannot execute query. " . end($dbh->errorInfo()));
+}
+
+function countentries( $dbh )
+{
+  $q = "SELECT COUNT(*) from Logs";
+  $result = $dbh->query( $q );
+  $row = $result->fetch(PDO::FETCH_NUM);
+  if( $row )
+    return $row[0];
+    
+  return 'FAIL';
+}
+
+function dbSchemaVersion( $dbh )
+{
+  $q = "SELECT logschema FROM Version";
+  $result = $dbh->query( $q );
+  $row = $result->fetch(PDO::FETCH_NUM);
+  if( $row )
+    return $row[0];
+  
+  // this shouldn't happen - no version entry in the table => fix it
+  $q = "INSERT INTO Version( logschema ) VALUES( -1 )";
+  $ok = $dbh->exec( $q );
+  if ($ok===false)
+    die("Cannot execute query. $q. " . end($dbh->errorInfo()));
+  return -1;
+}
+
+function update2to3( $dbh, $dbfile )
+{
+  global $usedDBdriver;
+  
+  if( 'sqlite' === $usedDBdriver )
+    return "Error: Database already updated!";
+  
+  $dbfileTmp = $dbfile . '.tmpMigration2to3';
+  if( file_exists($dbfileTmp) )
+  {
+    // migration already going on
+    if( time()-filemtime($dbfileTmp) > 600) {
+      // file older than 10 minutes
+      if( !unlink( $dbfileTmp ) )
+        die( "Error: can't clean up potentially broken migration!" );
+    } else {
+      // file younger than 10 minuts
+      die( "Error: already migrating!" );
+    }
+  }
+  $dbhTmp = new PDO('sqlite:' . $dbfileTmp) or die("cannot open the temp database with PDO(sqlite)");
+  create( $dbhTmp );
+  $result = retrieve( $dbh, '', NULL, NULL, true, 'ASC', false );
+  foreach ( $result as $row )
+    insert( $dbhTmp, $row['content'], $row['title'], explode(',', $row['tags']), $row['mapping'], $row['state'], '"' . $row['t'] . '"' );
+  
+  // transfer done => close DBs and move file
+  $dbh = null;
+  $dbhTmp = null;
+  rename( $dbfileTmp, $dbfile ) or die("Error: moving transfered database over old file");
+  
+  // return success
+  return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Show management informations
+function showInfo($dbh)
+{
+  global $dbfile, $usedDBdriver;
+?>
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>RSSLog Information</title>
+<link type="text/css" rel="stylesheet" href="../../cometvisu_management.css"></head><body>
+<h1>Information</h1>
+<ul>
+  <li>
+    Database file: <?php echo $dbfile; ?>
+  </li>
+  <li>
+    Active database driver: <?php echo $usedDBdriver; ?>
+  </li>
+  <li>
+    Available database drivers: <?php echo join( ', ', PDO::getAvailableDrivers() ); ?>
+  </li>
+  <li>
+    Schema version: <?php echo dbSchemaVersion( $dbh ); ?>
+  </li>
+<?php
+if( 'sqlite' !== $usedDBdriver )
+  echo '<li>Please update to latest database version by opening this <a href="?update">update page</a>.</li>';
+?>
+  <li>
+    Current number of entries in the database: <?php echo countentries($dbh); ?>
+  </li>
+  <li>
+    <a href="?dump">Show complete database content</a>.
+  </li>
+  <li>
+    <a href="?r=<?php echo strtotime("-1 week"); ?>">Delete entries older than 1 week</a>.
+  </li>
+  <li>
+    <a href="?r=<?php echo strtotime("-1 month"); ?>">Delete entries older than 1 month</a>.
+  </li>
+  <li>
+    <a href="?r=<?php echo strtotime("-1 year"); ?>">Delete entries older than 1 year</a>.
+  </li>
+</ul>
+</body></html>
+<?php
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Update database to new version
+function runUpdate($dbh)
+{
+  global $dbfile;
+?>
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>RSSLog Update</title>
+<link type="text/css" rel="stylesheet" href="../../cometvisu_management.css"></head><body>
+  <h1>Update</h1>
+  Updating database (might take a while): <?php $result = update2to3( $dbh, $dbfile ); echo $result === true ? 'Success' : $result; ?>
+</body></html>
+<?php
 }
 ?>
