0727d8e9dd8c41b8238ce1df66fac94582bde379
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-03-30 18:56:46 +0200

    Revert "changes in included config parts were not recognized by the caching. This changes the behaviour by loading the complete config file (load the parts and attach them to the config). So we have a complete config file with all parts included. The cache can detect changes for this file then in invalidate the cache if necessary."
    This reverts commit 063ae38d1a537d6f44cda4eb79f4c7ed0dc01217.

--- source/class/cv/Application.js
+++ source/class/cv/Application.js
@@ -301,9 +301,80 @@ qx.Class.define("cv.Application",
           // initialize NotificationCenter
           cv.ui.NotificationCenter.getInstance();
         }
-        var configLoader = new cv.util.ConfigLoader();
-        configLoader.load(this.bootstrap, this);
+        this.loadConfig();
       }, this);
+    },
+
+    /**
+     * Load a config file
+     */
+    loadConfig: function() {
+      // get the data once the page was loaded
+      var uri = qx.util.ResourceManager.getInstance().toUri('config/visu_config' + (cv.Config.configSuffix ? '_' + cv.Config.configSuffix : '') + '.xml');
+      if (cv.Config.testMode) {
+        // workaround for e2e-tests
+        uri = 'resource/config/visu_config' + (cv.Config.configSuffix ? '_' + cv.Config.configSuffix : '') + '.xml';
+      } else if (uri.indexOf("resource/") === -1) {
+        // unknown config, try to add the resource part manually
+        uri = uri.replace("config/", "resource/config/");
+      }
+      this.debug("Requesting "+uri);
+      var ajaxRequest = new qx.io.request.Xhr(uri);
+      ajaxRequest.set({
+        accept: "application/xml",
+        cache: !cv.Config.forceReload
+      });
+      ajaxRequest.setUserData("noDemo", true);
+      ajaxRequest.addListenerOnce("success", function (e) {
+        this.block(false);
+        var req = e.getTarget();
+        cv.Config.configServer = req.getResponseHeader("Server");
+        // Response parsed according to the server's response content type
+        var xml = req.getResponse();
+        if (xml && qx.lang.Type.isString(xml)) {
+          xml = qx.xml.Document.fromString(xml);
+        }
+
+        if (!xml || !xml.documentElement || xml.getElementsByTagName("parsererror").length) {
+          this.configError("parsererror");
+        }
+        else {
+          // check the library version
+          var xmlLibVersion = qx.bom.element.Attribute.get(qx.bom.Selector.query('pages', xml)[0], "lib_version");
+          if (xmlLibVersion === undefined) {
+            xmlLibVersion = -1;
+          }
+          if (cv.Config.libraryCheck && xmlLibVersion < cv.Config.libraryVersion) {
+            this.configError("libraryerror");
+          }
+          else {
+            if (req.getResponseHeader("X-CometVisu-Backend-LoginUrl")) {
+              cv.Config.backendUrl = req.getResponseHeader("X-CometVisu-Backend-LoginUrl");
+            }
+            if (req.getResponseHeader("X-CometVisu-Backend-Name")) {
+              cv.Config.backend = req.getResponseHeader("X-CometVisu-Backend-Name");
+            }
+            this.bootstrap(xml);
+          }
+        }
+      }, this);
+
+      ajaxRequest.addListener("statusError", function (e) {
+        var status = e.getTarget().getTransport().status;
+        if (!qx.util.Request.isSuccessful(status) && ajaxRequest.getUserData("noDemo")) {
+          ajaxRequest.setUserData("noDemo", false);
+          ajaxRequest.setUserData("origUrl", ajaxRequest.getUrl());
+          ajaxRequest.setUrl(ajaxRequest.getUrl().replace('config/', 'demo/'));
+          ajaxRequest.send();
+        } else if (!qx.util.Request.isSuccessful(status)) {
+          this.configError("filenotfound", [ajaxRequest.getUserData("origUrl"), ajaxRequest.getUrl()]);
+        }
+        else {
+          this.configError(status, null);
+        }
+      }, this);
+
+      ajaxRequest.send();
     },
 
     /**
@@ -505,6 +576,55 @@ qx.Class.define("cv.Application",
           cv.Config.initialPage = cv.TemplateEngine.getInstance().getPageIdByPath(startpage) || "id_";
         });
       }
+    },
+
+    /**
+     * Handle errors that occur during loading ot the config file
+     * @param textStatus {String} error status
+     * @param additionalErrorInfo {String} error message
+     */
+    configError: function( textStatus, additionalErrorInfo ) {
+      var configSuffix = (cv.Config.configSuffix ? cv.Config.configSuffix : '');
+      var title = qx.locale.Manager.tr('Config-File Error!');
+      var message = '';
+      switch (textStatus) {
+        case 'parsererror':
+          message = qx.locale.Manager.tr("Invalid config file!")+'<br/><a href="check_config.php?config=' + configSuffix + '">'+qx.locale.Manager.tr("Please check!")+'</a>';
+          break;
+        case 'libraryerror':
+          var link = window.location.href;
+          if (link.indexOf('?') <= 0) {
+            link = link + '?';
+          }
+          link = link + '&libraryCheck=false';
+          message = qx.locale.Manager.tr('Config file has wrong library version!')+'<br/>' +
+            qx.locale.Manager.tr('This can cause problems with your configuration')+'</br>' +
+            '<p>'+qx.locale.Manager.tr("You can run the %1Configuration Upgrader%2.", '<a href="./upgrade/index.php?config=' + configSuffix + '">', '</a>') +'</br>' +
+            qx.locale.Manager.tr('Or you can start without upgrading %1with possible configuration problems%2', '<a href="' + link + '">', '</a>')+'</p>';
+          break;
+        case 'filenotfound':
+          message = qx.locale.Manager.tr('404: Config file not found. Neither as normal config (%1) nor as demo config (%2).', additionalErrorInfo[0], additionalErrorInfo[1]);
+          break;
+        default:
+          message = qx.locale.Manager.tr('Unhandled error of type "%1"', textStatus);
+          if( additionalErrorInfo ) {
+            message += ': ' + additionalErrorInfo;
+          }
+          else {
+            message += '.';
+          }
+      }
+      var notification = {
+        topic: "cv.config.error",
+        title: title,
+        message: message,
+        severity: "urgent",
+        unique: true,
+        deletable: false
+      };
+      cv.core.notifications.Router.dispatchMessage(notification.topic, notification);
+      this.error(message.toString());
+      this.block(false);
     }
   }
 });
