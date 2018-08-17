5a6d6b541651c213cd41a09855a9364ce6660a6f
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-08-17 23:14:12 +0200

    get current path from argument

--- update.py
+++ update.py
@@ -10,15 +10,15 @@ import re
 from datetime import datetime
 import locale
 from zipfile import ZipFile
+from argparse import ArgumentParser
 
 locale.setlocale(locale.LC_ALL, '')
 
 NIGHTLY_PATH = 'https://dl.bintray.com/cometvisu/CometVisu/'
 RELEASE_PATH = 'https://api.github.com/repos/CometVisu/CometVisu/releases/latest'
-# TODO get path from parameter later
-CV_PATH = './build'
 
 sep_width = 70
+
 
 def get_latest_nightly():
     with urllib.request.urlopen(NIGHTLY_PATH) as response:
@@ -47,8 +47,8 @@ def get_latest_release():
         return release
 
 
-def get_installed_version():
-    with open(os.path.join(CV_PATH, 'version')) as f:
+def get_installed_version(current_dir):
+    with open(os.path.join(current_dir, 'version')) as f:
         return f.read()
 
 
@@ -57,7 +57,7 @@ def has_resource_folder(version):
     return int(match.group(1)) > 0 or int(match.group(2)) >= 11
 
 
-def install_version(cv, current_version):
+def install_version(cv, current_version, current_dir):
     # 1. Download + extract in temp dir
     os.makedirs('tmp', exist_ok=True)
 
@@ -82,7 +82,7 @@ def install_version(cv, current_version):
 
     # 2.1 copy config from old visu
     target_has_resource = has_resource_folder(cv['version'])
-    source_config_dir = os.path.join(CV_PATH, 'resource', 'config') if has_resource_folder(current_version) else os.path.join(CV_PATH, 'config')
+    source_config_dir = os.path.join(current_dir, 'resource', 'config') if has_resource_folder(current_version) else os.path.join(current_dir, 'config')
     target_config_dir = os.path.join(new_cv_dir, 'resource', 'config') if target_has_resource else os.path.join(new_cv_dir, 'config')
     print()
     print('-' * sep_width)
@@ -96,7 +96,7 @@ def install_version(cv, current_version):
         return
 
     # 2.2 copy design customs from old visu
-    for file in glob.glob(os.path.join(CV_PATH, '**', 'designs', '*', 'custom.css')):
+    for file in glob.glob(os.path.join(current_dir, '**', 'designs', '*', 'custom.css')):
         match = re.search('.+%sdesigns%s(.+)%scustom\.css$' % (os.path.sep, os.path.sep, os.path.sep), file)
         dest_dir = os.path.join(new_cv_dir, 'resource', 'designs', match.group(1)) if target_has_resource else os.path.join(new_cv_dir, 'designs', match.group(1))
         print(' - copying %s to %s' % (file, dest_dir))
@@ -109,35 +109,41 @@ def install_version(cv, current_version):
     print('>>> Replacing old installation')
     backup_dir = os.path.join('cv-backup-%s' % time.time())
     print(' - backup old installation to %s' % backup_dir)
-    shutil.move(CV_PATH, backup_dir)
+    shutil.move(current_dir, backup_dir)
 
     # 4. move new visu to old position
-    print(' - move new installation to %s' % CV_PATH)
-    shutil.move(new_cv_dir, CV_PATH)
+    print(' - move new installation to %s' % current_dir)
+    shutil.move(new_cv_dir, current_dir)
     print('-' * sep_width)
 
     print('')
-    print('>>> DONE. CometVisu version %s has been installed into %s!' % (cv['version'], CV_PATH))
+    print('>>> DONE. CometVisu version %s has been installed into %s!' % (cv['version'], current_dir))
 
 
 if __name__ == '__main__':
-    current = get_installed_version()
-    nightly = get_latest_nightly()
-    release = get_latest_release()
-    print('\t\t\t\tVersion\t\tDate')
-    print('-' * sep_width)
-    print('Currently installed version:\t{0}'.format(current))
-    print('Latest nightly build:\t\t{:}\t{:%x %X}'.format(nightly['version'], nightly['date']))
-    print('Latest release:\t\t\t{:}\t\t{:%x %X}'.format(release['version'], release['date']))
-
-    # check if the current version is upgradable and let the user decide wich version to use
-    print('Please choose which CometVisu version should be installed:')
-    action = input('(0) do nothing\n(1) latest release\n(2) latest nightly build\nPlease enter number: ')
-    if action == "1":
-        install_version(release, current)
-    elif action == "2":
-        install_version(nightly, current)
-    elif action == "0":
-        pass
+    parser = ArgumentParser(usage="%(prog)s - CometVisu update script")
+    parser.add_argument('current', type=str, help='path to your current CometVisu installation', nargs='?')
+    options, unknown = parser.parse_known_args()
+    if options.current is None:
+        print('you have to specify a path to your current CometVisu installation')
     else:
-        print('unknown option: %s' % action)
+        current = get_installed_version(options.current)
+        nightly = get_latest_nightly()
+        release = get_latest_release()
+        print('\t\t\t\tVersion\t\tDate')
+        print('-' * sep_width)
+        print('Currently installed version:\t{0}'.format(current))
+        print('Latest nightly build:\t\t{:}\t{:%x %X}'.format(nightly['version'], nightly['date']))
+        print('Latest release:\t\t\t{:}\t\t{:%x %X}'.format(release['version'], release['date']))
+
+        # check if the current version is upgradable and let the user decide wich version to use
+        print('Please choose which CometVisu version should be installed:')
+        action = input('(0) do nothing\n(1) latest release\n(2) latest nightly build\nPlease enter number: ')
+        if action == "1":
+            install_version(release, current, options.current)
+        elif action == "2":
+            install_version(nightly, current, options.current)
+        elif action == "0":
+            pass
+        else:
+            print('unknown option: %s' % action)
