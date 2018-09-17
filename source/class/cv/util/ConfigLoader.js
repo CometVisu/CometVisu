ebad48d0b16146a3c316378b9bb75a6d4aa4fe69
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-09-17 21:35:16 +0200

    remove the real url

--- source/class/cv/util/ConfigLoader.js
+++ source/class/cv/util/ConfigLoader.js
@@ -64,7 +64,7 @@ qx.Class.define('cv.util.ConfigLoader', {
         }
         this.__xml = xml;
         qx.bom.Selector.query('include', xml).forEach(this.loadInclude, this);
-        this.__loadQueue.remove(uri);
+        this.__loadQueue.remove(ajaxRequest.getUrl());
 
         if (!xml || !xml.documentElement || xml.getElementsByTagName("parsererror").length) {
           this.configError("parsererror");
