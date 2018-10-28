6498bd090ef37eff346e35aff7af3414b328ef80
Christian Mayer <ChristianMayer@users.noreply.github.com>
2018-10-28 17:41:26 +0100

    Change implicit to explicit arg passing

--- utils/travis/docker_push.sh
+++ utils/travis/docker_push.sh
@@ -21,7 +21,7 @@ echo "building docker container for ${IMAGE_NAME}:${VERSION_TAG},${MASTER_TAG},$
 BUILD_DATE=`date --iso-8601=seconds`
 VCS_REF=`git rev-parse --short HEAD`
 
-docker build -t $IMAGE_NAME:$VERSION_TAG --build-arg BUILD_DATE --build-arg VCS_REF --build-arg VERSION_TAG --build-arg TRAVIS_JOB_NUMBER --build-arg TRAVIS_JOB_WEB_URL --build-arg TRAVIS_BUILD_WEB_URL .
+docker build -t $IMAGE_NAME:$VERSION_TAG --build-arg BUILD_DATE="$BUILD_DATE" --build-arg VCS_REF="$VCS_REF" --build-arg VERSION_TAG="$VERSION_TAG" --build-arg TRAVIS_JOB_NUMBER --build-arg TRAVIS_JOB_WEB_URL --build-arg TRAVIS_BUILD_WEB_URL .
 docker tag "${IMAGE_NAME}:${VERSION_TAG}" "${IMAGE_NAME}:${MASTER_TAG}"
 docker push "${IMAGE_NAME}:${MASTER_TAG}"
 docker push "${IMAGE_NAME}:${VERSION_TAG}"
