62004b13032d2b48f7a853a09ac7f9910a67711d
ChristianMayer <mail@christianmayer.de>
2019-01-12 00:28:29 +0100

    Fix diagram plugin InfluxDB backend
    - Make also the data fetch aware of self signed TLS
    - Make a bit more robust when the config is not good

--- source/resource/plugins/diagram/influxfetch.php
+++ source/resource/plugins/diagram/influxfetch.php
@@ -56,6 +56,14 @@ function query( $q, $db = '', $auth )
         'header' => "Authorization: Basic " . base64_encode( $hidden[$influxKey]['user'] . ':' . $hidden[$influxKey]['pass'] )
       )
     );
+    if( array_key_exists( 'selfsigned', $hidden[$influxKey] ) &&
+      'true' == $hidden[$influxKey]['selfsigned']  ) {
+      $opts['ssl'] = [
+        'verify_peer' => false,
+        'allow_self_signed' => true,
+        'verify_peer_name' => false
+      ];
+    }
 
     $context = stream_context_create( $opts );
   }
@@ -110,7 +118,10 @@ function getTs( $tsParameter, $field, $start, $end, $ds, $res, $fill, $filter )
 
   if( $filter )
   {
-    $filter = 'AND ' . str_replace( "\\'", "'", $filter );
+    if( '()' == $filter )
+      $filter = '';
+    else
+      $filter = 'AND ' . str_replace( "\\'", "'", $filter );
   }
 
   if( '' != $res )
