05ba59eaf4161f5ac221417d5d6ba9a951ee511d
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-01-20 18:56:48 +0100

    remove console.log

--- source/resource/demo/media/demo_2d_backdrop_red_pot.js
+++ source/resource/demo/media/demo_2d_backdrop_red_pot.js
@@ -16,7 +16,6 @@ client.update = function(json ) // overload the handler
   var filling = qx.bom.Selector.query('#rect3855', document)[0];
   filling.y.baseVal.value=200.57388 + (100-h)*2;
   filling.height.baseVal.value = h*2;
-  console.log(h);
   qx.bom.element.Attribute.set(qx.bom.Selector.query('#path3029-4', document)[0], 'd', 'm 524.85653,'+(200.57388+ (100-h)*2)+' a 100,37.795274 0 0 1 -200,0 100,37.795274 0 1 1 200,0 z');
 };
   
