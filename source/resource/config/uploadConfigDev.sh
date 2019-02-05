dfb46096356eec5b590e79fafff00ccfda9b89f1
Matthias Werner <werner.matthias@web.de>
2019-02-05 22:52:58 +0100

    Update Upload-Script
    Alle notwendigen Dateien werden per script zum Wiregate geladen

---
+++ source/resource/config/uploadConfigDev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#kopieren
+scp /media/develop/CometVisu_dev/source/resource/config/visu*.xml root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
+scp /media/develop/CometVisu_dev/source/resource/config/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
+
+scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/owm/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/owm
+scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/
+scp /media/develop/CometVisu_dev/source/resource/config/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
+
+echo "Config hochgeladen..."
