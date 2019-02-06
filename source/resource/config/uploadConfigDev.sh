a471b50195c31de1a4f798a3b3bbf4a64e0ca0a5
Matthias Werner <werner.matthias@web.de>
2019-02-06 22:14:24 +0100

    Anpassung UploadScript

--- source/resource/config/uploadConfigDev.sh
+++ source/resource/config/uploadConfigDev.sh
@@ -3,9 +3,10 @@
 #kopieren
 scp /media/develop/CometVisu_dev/source/resource/config/visu*.xml root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
 scp /media/develop/CometVisu_dev/source/resource/config/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
+scp /media/develop/CometVisu_dev/source/resource/config/media/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/config/media
 
 scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/owm/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/owm
 scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/
-scp /media/develop/CometVisu_dev/source/resource/config/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
+#scp /media/develop/CometVisu_dev/source/resource/config/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
 
 echo "Config hochgeladen..."
