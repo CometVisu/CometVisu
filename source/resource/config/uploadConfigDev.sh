82c41a56c2ba3c7d79b35677cddca49d86ef3a70
Matthias Werner <werner.matthias@web.de>
2019-02-06 23:16:25 +0100

    Update des Upload Script

--- source/resource/config/uploadConfigDev.sh
+++ source/resource/config/uploadConfigDev.sh
@@ -4,6 +4,8 @@
 scp /media/develop/CometVisu_dev/source/resource/config/visu*.xml root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
 scp /media/develop/CometVisu_dev/source/resource/config/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/config
 scp /media/develop/CometVisu_dev/source/resource/config/media/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/config/media
+scp /media/develop/CometVisu_dev/source/class/cv/plugins/OpenweatherMap.js root@wiregate1769:/var/www/cometvisu11dev/source/class/cv/plugins/
+
 
 scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/owm/*.js root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/owm
 scp /media/develop/CometVisu_dev/source/resource/plugins/openweathermap/*.css root@wiregate1769:/var/www/cometvisu11dev/source/resource/plugins/openweathermap/
