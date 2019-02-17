75a9e8b78587eb337d201402194531c16351d0f8
Travis CI <generator@cometvisu.org>
2019-02-17 17:46:25 +0000

    Deploy to GitHub Pages: b45249a54c03c0a73badcb754a4d5b00b782a4d9

--- de/0.11/demo-source/source/class/cv/Version.js
+++ de/0.11/demo-source/source/class/cv/Version.js
@@ -3,9 +3,9 @@ qx.Class.define("cv.Version", {
   type: "static",
 
   statics: {
-    REV: "0a9c1281f6a7908dd1196016a183caab9ece1787",
+    REV: "b45249a54c03c0a73badcb754a4d5b00b782a4d9",
     BRANCH: "HEAD",
     VERSION: "0.11.0-RC2",
-    DATE: "2019-02-17T15:30:15.600151"
+    DATE: "2019-02-17T17:45:42.551234"
   }
 });
