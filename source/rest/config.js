fbf0ae8feaf646e5b21d18a6b3c3cd8acd8c8ec6
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-03-17 18:58:33 +0100

    remove test mount again

--- source/rest/config.js
+++ source/rest/config.js
@@ -9,11 +9,6 @@ module.exports = {
       path: path.resolve(path.join(__dirname, '..', 'resource', 'demo')),
       showSubDirs: false,
       writeable: false
-    }, {
-      mountPoint: 'rest',
-      path: path.resolve(__dirname),
-      showSubDirs: true,
-      writeable: true
     }
   ]
 }
