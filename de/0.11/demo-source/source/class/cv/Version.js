372c30cb287d2c390f35cdba155c8c22e7dc6ff8
Travis CI <generator@cometvisu.org>
2019-04-09 21:52:47 +0000

    Deploy to GitHub Pages: 1287f5d817fb3c703c74d3e903eff157c4d4428a

--- de/0.11/demo-source/source/class/cv/Version.js
+++ de/0.11/demo-source/source/class/cv/Version.js
@@ -3,9 +3,9 @@ qx.Class.define("cv.Version", {
   type: "static",
 
   statics: {
-    REV: "5a2cae8f97e6b74b7dbc58fa32264b89e63ffe1a",
+    REV: "1287f5d817fb3c703c74d3e903eff157c4d4428a",
     BRANCH: "HEAD",
     VERSION: "0.11.0",
-    DATE: "2019-04-06T12:32:14.064631"
+    DATE: "2019-04-09T21:51:59.626503"
   }
 });
---
+++ de/0.11/manual/_sources/tutorial/index.txt
@@ -0,0 +1,12 @@
+***********************
+Bedienung der CometVisu
+***********************
+
+-  :doc:`Tutorial: Erste Schritte - eine Visu in einer halbe Minute <first_steps>`
+
+.. toctree::
+    :hidden:
+
+    first_steps
+
+
