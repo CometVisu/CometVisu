c2bb0b31d217a8c7971407a15d35330c047e9fe8
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-05-11 09:55:03 +0200

    remove object reference in destructor

--- source/class/cv/plugins/ColorChooser.js
+++ source/class/cv/plugins/ColorChooser.js
@@ -244,5 +244,14 @@ qx.Class.define('cv.plugins.ColorChooser', {
     // register the parser
     cv.parser.WidgetParser.addHandler("colorchooser", statics);
     cv.ui.structure.WidgetFactory.registerClass("colorchooser", statics);
+  },
+
+  /*
+  ***********************************************
+    DESTRUCTOR
+  ***********************************************
+  */
+  destruct: function () {
+    this.__farbtastic = null;
   }
 });
