167cf2f6fc6963e35305f566389da3a3a34ca9c5
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-10-27 22:26:31 +0100

    add custom build steps needed

--- compile.js
+++ compile.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const fse = require('fs-extra')
 const path = require('path')
+const { exec } = require('child_process');
 
 qx.Class.define("cv.compile.LibraryApi", {
   extend: qx.tool.cli.api.LibraryApi,
@@ -13,6 +14,10 @@ qx.Class.define("cv.compile.LibraryApi", {
       const config = this.getCompilerApi().getConfiguration()
       this.readEnv(config)
 
+      if (config.targetType === 'build') {
+        this.beforeBuild()
+      }
+
       let command = this.getCompilerApi().getCommand();
       command.addListener("made", () => this._onMade());
     },
@@ -23,6 +28,10 @@ qx.Class.define("cv.compile.LibraryApi", {
     _onMade() {
       const config = this.getCompilerApi().getConfiguration()
       this.copyFiles(config)
+
+      if (config.targetType === 'build') {
+        this.afterBuild(config)
+      }
     },
 
     readEnv (config) {
@@ -51,22 +60,56 @@ qx.Class.define("cv.compile.LibraryApi", {
         "../node_modules/monaco-editor"
       ]
       const currentDir = process.cwd()
-      let targetDir = ""
-      config.targets.some(target => {
-        if (target.type === config.targetType) {
-          targetDir = target.outputPath
-        }
-      })
+      const targetDir = this._getTargetDir(config, config.targetType)
       if (targetDir) {
         filesToCopy.forEach(file => {
           const source = path.join(currentDir, 'source', file)
           const target = path.join(currentDir, targetDir, (file.startsWith('../') ? file.substring(3) : file))
           const stats = fs.statSync(source);
-          const dirname = stats.isDirectory()? target : path.dirname(target)
+          const dirname = stats.isDirectory() ? target : path.dirname(target)
           fse.ensureDirSync(dirname)
           fse.copySync(source, target)
         })
       }
+
+      // copy IconConfig.js to make it available for resource/icon/iconlist.html
+      const classTargetDir = path.join(currentDir, targetDir, 'class', 'cv')
+      fse.ensureDirSync(classTargetDir)
+      fse.copySync(path.join(process.cwd(), 'source', 'class', 'cv', 'IconConfig.js'), path.join(classTargetDir, 'IconConfig.js'))
+    },
+
+    /**
+     * Executed before compiling a build version
+     */
+    beforeBuild () {
+      exec('utils/update_version.py')
+    },
+
+    /**
+     * Executed after the build version has been compiled
+     */
+    afterBuild (config) {
+      // build-libs
+      console.log('uglifying libraries')
+      exec('grunt uglify:libs')
+
+      // build-append-plugin-libs
+      console.log('appending libraries to plugins')
+      exec('./cv build -bp"')
+
+      // build-paths
+      console.log('build paths')
+      exec('./cv build -up')
+    },
+
+    _getTargetDir (config, type) {
+      let targetDir = null
+      config.targets.some(target => {
+        if (target.type === type) {
+          targetDir = target.outputPath
+        }
+      })
+      return targetDir
     }
   }
 });
