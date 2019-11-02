5f1ffe28ac96325d9619bed78b90efa5793e5d5d
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-11-02 10:40:48 +0100

    fix initialization of speech plugin in cached mode and wait for voices to be available

--- source/class/cv/plugins/Speech.js
+++ source/class/cv/plugins/Speech.js
@@ -92,7 +92,9 @@ qx.Class.define('cv.plugins.Speech', {
         'address' : address,
         'mapping' : element.getAttribute('mapping'),
         'repeatTimeout': element.getAttribute('repeat-timeout') ? parseInt(element.getAttribute('repeat-timeout')) : -1,
-        '$$type'  : 'speech'
+        '$$type'  : 'speech',
+        // this widget needs to be initialized when the cache is used, otherwise it wont be available
+        '$$initOnCacheLoad': true
       });
     }
   },
@@ -105,6 +107,7 @@ qx.Class.define('cv.plugins.Speech', {
   properties: {
     path              : { check: "String" },
     $$type            : { check: "String" },
+    $$initOnCacheLoad : { check: "Boolean" },
     language          : { check: "String" },
     mapping           : { check: "String", init: "" },
     repeatTimeout     : { check: "Number", init: -1 },
