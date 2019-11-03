d89c84e96c1d024430b07347b98e8d898cc8cdc2
Tobias Bräutigam <tbraeutigam@gmail.com>
2019-11-03 15:30:22 +0100

    fix merge conflict

--- source/resource/config/hidden.php
+++ source/resource/config/hidden.php
@@ -1,13 +1,9 @@
 <?php
 // File for configuraions that shouldn't be shared with the user
 $hidden = array(
-<<<<<<< HEAD
-  'fritzbox' => array('test' => 'value')
-=======
   'fritzbox' => array( 'uri' => 'https://192.168.0.1:49443/', 'user' => 'CometVisuTestUser', 'pass' => 'cvtu4here' ),
   //'fritzbox' => array( 'uri' => 'http://192.168.0.1:49000/', 'user' => 'CometVisuTestUser', 'pass' => 'cvtu4here' ),
   //'fritzbox' => array( 'uri' => 'https://fritz.box:49443/', 'user' => 'CometVisuTestUser', 'pass' => 'cvtu4here' , 'selfsigned' => 'true'),
   'influx' => array('uri' => 'https://172.17.0.1/proxy/ts/query', 'user' => 'docker', 'pass' => 'cwkvJPQeFlGlRKc402CA', 'selfsigned' => 'true')
->>>>>>> develop
 );
 ?>
