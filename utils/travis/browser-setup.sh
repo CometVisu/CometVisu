c055d8f4743d7c68b6a9174a5c729c6efc6780e0
Tobias Bräutigam <tbraeutigam@gmail.com>
2021-02-07 08:46:04 +0100

    quit download to reduce Ci log lines

--- utils/travis/browser-setup.sh
+++ utils/travis/browser-setup.sh
@@ -7,10 +7,10 @@ if [ "$CV_BROWSER" = "" ]; then
 fi
 
 if [ $CV_BROWSER = Firefox ]; then
-    wget -O /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-${CV_VERSION}&lang=en-US&os=linux64"
+    wget -q -O /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-${CV_VERSION}&lang=en-US&os=linux64"
     tar xf /tmp/firefox.tar.bz2
 else
-    wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-${CV_VERSION}_current_amd64.deb
+    wget -q -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-${CV_VERSION}_current_amd64.deb
     dpkg --extract /tmp/chrome.deb chrome-x
     if [ $CV_VERSION = stable ]; then
         mv chrome-x/opt/google/chrome chrome
