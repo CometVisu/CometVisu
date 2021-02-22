4a95ad5ab4c362feb5396c9c7f37bf879d2e1dc7
undefined <undefined>
2021-02-22 18:26:49 +0100

    migrate tagger to action

--- .github/actions/github/index.js
+++ .github/actions/github/index.js
@@ -45,6 +45,7 @@ class GithubClient {
     this.owner = owner;
     this.repo = repo;
     this.versionRegex = new RegExp(`^${this.getVersion()}([0-9]+)$`, 'g');
+    this.maxTextLength = 70
 
     this.__cache = {};
   }
@@ -85,10 +86,13 @@ class GithubClient {
     }
   }
 
-  async getLatestTag() {
+  async getLatestTag(baseVersion) {
     const git = simpleGit();
+    if (!baseVersion) {
+      baseVersion = this.getVersion();
+    }
     const res = await git.raw('for-each-ref', '--format=%(refname)', '--sort=-taggerdate', 'refs/tags');
-    const regex = new RegExp(`^refs/tags/${this.getVersion()}(\\d+)$`);
+    const regex = new RegExp(`^refs/tags/${baseVersion}(\\d+)$`);
     let tag = null;
     res.split('\n').some( line => {
       if (regex.test(line)) {
@@ -118,9 +122,9 @@ class GithubClient {
       multiLine: true,
       symmetric: false
     }
-    console.debug(args);
+    //console.debug(args);
     const raw = await git.log(args);
-    console.log(raw.total, "changes found");
+    console.log(raw.total, "changes found from", startRef, "to", endRef);
     return raw.total > 0;
   }
 
@@ -154,6 +158,11 @@ class GithubClient {
     return null;
   }
 
+  /**
+   * Keep only the latest `limit` CometVisu release builds as assets in a release.
+   * @param limit {Number} amount of builds to keep
+   * @returns {Promise<null>}
+   */
   async cleanupAssets(limit) {
     const release = await this.getLatestNightlyBuild();
     const zipBuildAssets =release.assets
@@ -175,16 +184,22 @@ class GithubClient {
     }
     if (tarBuildAssets.length > limit) {
       for (let i = limit; i < tarBuildAssets.length; i++) {
-        await this.client.request('DELETE /repos/{owner}/{repo}/releases/assets/{assetId}', {
+        await this.client.deleteReleaseAsset({
           owner: this.owner,
           repo: this.repo,
-          assetId: tarBuildAssets[i].id
+          asset_id: tarBuildAssets[i].id
         });
         console.log("Deleting", tarBuildAssets[i].name)
       }
     }
   }
 
+  /**
+   * Download fil (e.g an asset from an release)
+   * @param dest {string} path to store the file (including filename)
+   * @param url {string} URL to download the file from
+   * @returns {Promise<null>}
+   */
   async download(dest, url) {
     const file = fs.createWriteStream(dest);
     return new Promise((resolve, reject) => {
@@ -198,6 +213,174 @@ class GithubClient {
         reject(err);
       });
     });
+  }
+
+
+  /**
+   * Get title + body from pull request
+   * @param pullId {Number} id of the pull request
+   * @returns {string}
+   */
+  async getRequestDescription(pullId) {
+    const pr = await this.client.pulls.get({
+      owner: 'CometVisu',
+      repo: this.repo,
+      pull_number: pullId
+    });
+    let text = pr.data.title.trim();
+    const body = pr.data.body.trim().split("\n").filter(line => !/^s*Signed-off-by:.+$/.test(line))
+    if (text.endsWith('…') && body.length > 0 && body[0].startsWith('…')) {
+      text = text.substr(0, text.length-1) + body.pop().substr(1).trim();
+    }
+    return `- [#${pullId}](${pr.data.html_url}) ${text.length > this.maxTextLength ? text.substr(0, this.maxTextLength) + '…' : text} (@${pr.data.user.login})\n`;
+  }
+
+  /**
+   * Collect information for all pull requests that have been merged since "ref"
+   * @param ref {string} Tag or commit SHA
+   * @returns {Promise<string>}
+   */
+  async getMergeInfo(ref) {
+    const git = simpleGit();
+    const endRef = await git.revparse(['--abbrev-ref', 'HEAD']);
+    const out = await git.log(['--merges', `${ref}..${endRef}`]);
+    let text = ''
+    for (const entry of out.all) {
+      const match = /^Merge pull request #([\d]+) from.+$/.exec(entry.message);
+      if (match) {
+        const info = await this.getRequestDescription(parseInt(match[1]));
+        text += info;
+      }
+    }
+    return text
+  }
+
+  async increaseBuildTag(start, dryRun) {
+    if (typeof start === 'string') {
+      start = start === 'true';
+    } else if (typeof start !== 'boolean') {
+      start = false;
+    }
+    if (typeof dryRun === 'string') {
+      dryRun = dryRun === 'true';
+    } else if (typeof start !== 'boolean') {
+      dryRun = false;
+    }
+    let baseVersion = this.getVersion();
+    if (/.\d+$/.test(baseVersion)) {
+      //end with number -> avoid v0.12.01 tags, we want v0.12.0-1
+      baseVersion += "-";
+    }
+    let latestTag = await this.getLatestTag(baseVersion);
+    let useLastRelease = false;
+
+    if (!latestTag) {
+      if (!start) {
+        core.setFailed(`Need a manual starting tag for ${baseVersion} - aborting`);
+        return;
+      } else {
+        const latestRelease = await this.client.repos.getLatestRelease({
+          owner: this.owner,
+          repo: this.repo,
+        });
+        latestTag = `refs/tags/${latestRelease.tag_name}`;
+        useLastRelease = true;
+      }
+    }
+    const git = simpleGit();
+    const lastTagHash = await git.raw(['rev-list', '-n', '1', latestTag]);
+    const currentHash = await git.revparse(['HEAD']);
+
+    const re = new RegExp(`^${baseVersion}(\\d+)$`)
+    const m = re.exec(latestTag.split('/')[2]);
+    let buildNo = 0
+    if (!m) {
+      if (!start) {
+        core.setFailed(`Unable to parse previous tag '${info.split('/')[2]}'`);
+        return;
+      }
+    } else {
+      buildNo = parseInt(m[1]);
+    }
+
+    const newRev = `${baseVersion}${buildNo + 1}`;
+
+    if (currentHash === lastTagHash) {
+      console.log('No new commits - skipping');
+      return;
+    } else {
+      const hasChanges = await this.checkForChanges(latestTag);
+      if (!hasChanges) {
+        console.log('No changes in source folder - skipping');
+        return;
+      }
+    }
+
+    console.log('New commits detected - tagging new dev release:', newRev);
+
+    const branch = await git.revparse(['--abbrev-ref', 'HEAD']);
+
+    let buildInfo = `
+---
+
+Branch       : ${branch}
+Commit       : ${currentHash}
+`;
+
+    let tagDescription = ""
+    const info = !useLastRelease ? await this.getMergeInfo(latestTag) : ''
+    if (info) {
+      tagDescription += 'This release comes with these annotated changes:\n\n'
+      tagDescription += info
+    } else {
+      tagDescription += 'This release contains minor fixes or improvements.\n'
+    }
+    git.addConfig("user.name", process.env.COMMIT_AUTHOR_NAME);
+    git.addConfig("user.email", process.env.COMMIT_AUTHOR_EMAIL);
+
+    if (!dryRun) {
+      await git.addAnnotatedTag(newRev, tagDescription + buildInfo);
+    }
+
+    console.log("Pushing tags...");
+    if (!dryRun) {
+      await git.pushTags('origin')
+    }
+    console.log("Creating release...");
+    let releaseName = newRev;
+    let prerelease = false;
+    const draft = false;
+    let changes = `The latest changes can be seen in the [change log](https://raw.githubusercontent.com/${this.owner}/${this.repo}/${newRev}/ChangeLog).`;
+
+    if (baseVersion.endsWith("dev")) {
+      releaseName = `CometVisu nightly build ${newRev}`;
+      prerelease = true;
+      changes = tagDescription;
+    } else if (baseVersion.endsWith("RC")) {
+      releaseName = `CometVisu release ${baseVersion.substr(0, baseVersion.length - 3)} - release candidate ${newRev.substr(baseVersion.length)}`
+      prerelease = true;
+    }
+
+    let releaseMessage=`
+The CometVisu project is happy to publish the version ${newRev} that can be downloaded at
+[https://github.com/${this.owner}/${this.repo}/releases/tag/${newRev}](https://github.com/${this.owner}/${this.repo}/releases/tag/${newRev}).
+
+${changes}
+    `;
+
+    if (!dryRun) {
+      await this.client.repos.createRelease({
+        owner: this.owner,
+        repo: this.repo,
+        tag_name: newRev,
+        name: releaseName,
+        body: releaseMessage,
+        draft: draft,
+        prerelease: prerelease
+      });
+    } else {
+      console.log(`would have created new git release '${releaseName}' from tag '${newRev}', prerelease=${prerelease}\n\n${releaseMessage}`);
+    }
   }
 }
 
