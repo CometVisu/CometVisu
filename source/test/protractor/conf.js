cad474cf30f328af9f8f5177af17c5a6f1e5141a
undefined <undefined>
2021-04-03 14:46:31 +0200

    fix tests

--- source/test/protractor/conf.js
+++ source/test/protractor/conf.js
@@ -11,9 +11,8 @@ exports.config = {
   directConnect: true,
 
   suites: {
-    // common: ['specs/d*spec.js'],
-    // widgets: ['specs/widgets/*spec.js']
-    widgets: ['specs/widgets/Navbar-spec.js']
+    common: ['specs/d*spec.js'],
+    widgets: ['specs/widgets/*spec.js']
   },
   capabilities: {
     browserName: 'chrome',
