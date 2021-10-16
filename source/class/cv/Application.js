88da4110c8d94e8241ab450e2342da8414b0737e
tbraeutigam@gmail.com <tbraeutigam@gmail.com>
2021-10-16 20:37:36 +0200

    test with error

--- source/class/cv/Application.js
+++ source/class/cv/Application.js
@@ -46,7 +46,8 @@ qx.Class.define("cv.Application",
       qx.io.PartLoader.require([lang]);
     }
 
-    qx.bom.PageVisibility.getInstance().addListener("change", function () {
+    qx.bom.PageVisibility.getInstance().addListener('change', function () {
+
       this.setActive(qx.bom.PageVisibility.getInstance().getVisibilityState() === "visible");
     }, this);
 
