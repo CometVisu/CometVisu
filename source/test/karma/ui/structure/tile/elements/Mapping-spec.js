4e41d956a837e8a0cbd38b66dc799fa1f48f2d66
tbraeutigam@gmail.com <tbraeutigam@gmail.com>
2021-11-25 18:29:02 +0100

    test default values

--- source/test/karma/ui/structure/tile/elements/Mapping-spec.js
+++ source/test/karma/ui/structure/tile/elements/Mapping-spec.js
@@ -23,6 +23,8 @@ describe('testing the <cv-mapping> component', function() {
     mapping.innerHTML = `
         <entry value="1">ON</entry>
         <entry value="0">OFF</entry>
+        <entry value="NULL">is_null</entry>
+        <entry value="*">wildcard</entry>
     `;
     document.body.appendChild(mapping);
     const map = mapping._instance;
@@ -31,6 +33,30 @@ describe('testing the <cv-mapping> component', function() {
     expect(map.getConnected()).toBeTruthy();
     expect(map.mapValue('1')).toEqual('ON');
     expect(map.mapValue('0')).toEqual('OFF');
+    expect(map.mapValue(null)).toEqual('is_null');
+    expect(map.mapValue('anything')).toEqual('wildcard');
+
+    mapping.remove();
+
+    expect(map.getConnected()).toBeFalsy();
+  });
+
+  it('should test if the default value is used', function() {
+    const mapping = document.createElement('cv-mapping');
+    mapping.setAttribute('name', 'test');
+
+    mapping.innerHTML = `
+        <entry value="1" default="true">ON</entry>
+        <entry value="0">OFF</entry>
+    `;
+    document.body.appendChild(mapping);
+    const map = mapping._instance;
+
+    expect(map).toBeInstanceOf(cv.ui.structure.tile.elements.Mapping);
+    expect(map.getConnected()).toBeTruthy();
+    expect(map.mapValue('1')).toEqual('ON');
+    expect(map.mapValue('0')).toEqual('OFF');
+    expect(map.mapValue('anything')).toEqual('ON');
 
     mapping.remove();
 
@@ -45,7 +71,7 @@ describe('testing the <cv-mapping> component', function() {
         <entry range_min="-30" range_max="0">freezing</entry>
         <entry value="0">zero</entry>
         <entry range_min="0" range_max="10">cold</entry>
-        <entry range_min="10" range_max="25">warm</entry>
+        <entry range_min="11" range_max="25" default="true">warm</entry>
         <entry range_min="25" range_max="50">hot</entry>
     `;
     document.body.appendChild(mapping);
@@ -58,6 +84,9 @@ describe('testing the <cv-mapping> component', function() {
     expect(map.mapValue('0')).toEqual('zero');
     expect(map.mapValue('25')).toEqual('warm');
     expect(map.mapValue('26')).toEqual('hot');
+
+    // test is default is used
+    expect(map.mapValue('99')).toEqual('warm');
 
     mapping.remove();
 
