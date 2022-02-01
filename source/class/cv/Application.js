d9856307358b71c6b779b8127992b542fee18e75
Tobias Bräutigam <tbraeutigam@gmail.com>
2022-02-01 17:23:48 +0100

    make size calculations more robust and also make sure that screensize is not invalidated before the basic strcuture has been loaded
    closes #1180

--- source/class/cv/Application.js
+++ source/class/cv/Application.js
@@ -41,6 +41,7 @@ qx.Class.define('cv.Application',
   */
   construct: function () {
     this.base(arguments);
+    this.__appReady = false;
     this.initCommandManager(new qx.ui.command.GroupManager());
     const lang = qx.locale.Manager.getInstance().getLanguage();
     if (qx.io.PartLoader.getInstance().hasPart(lang)) {
@@ -201,6 +202,7 @@ qx.Class.define('cv.Application',
   members :
   {
     _blocker: null,
+    __appReady: null,
 
     /**
      * Toggle the {@link qx.bom.Blocker} visibility
@@ -604,7 +606,7 @@ qx.Class.define('cv.Application',
       if (cv.Config.mobileDevice === undefined) {
         this.setMobile(window.innerWidth < cv.Config.maxMobileScreenWidth);
       }
-      if (!init) {
+      if (!init && this.__appReady) {
         cv.ui.layout.ResizeHandler.invalidateScreensize();
       }
     },
@@ -743,7 +745,10 @@ qx.Class.define('cv.Application',
               cv.ConfigCache.dump(xml, xmlHash);
             }, this);
           }
+          this.__appReady = true;
         }.bind(this));
+      } else {
+        this.__appReady = true;
       }
     },
 
