4242720d932dc04e39ab44f8e294fd31338a65ac
undefined <undefined>
2022-02-01 22:54:55 +0100

    Please the linter

--- source/class/cv/IconHandler.js
+++ source/class/cv/IconHandler.js
@@ -111,7 +111,7 @@ qx.Class.define('cv.IconHandler', {
      */
     get: function (name, type = '*', flavour = '*', color = '*') {
       if (!this.__db[name]) {
-        return (a,b,c,asText)=>asText?'[unknown]':document.createTextNode('[unknown]');
+        return (a, b, c, asText) => asText?'[unknown]':document.createTextNode('[unknown]');
       }
       if (!this.__db[name][type]) {
         type = '*'; // undefined -> use default
