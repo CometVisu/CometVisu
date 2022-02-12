5fe1d252481bdf2d6f56c3e050df0f56ee346d51
Tobias Bräutigam <tbraeutigam@gmail.com>
2022-02-12 09:01:57 +0100

    during replay of a recorded session this information might not be available
    (cherry picked from commit 4fd251c0ab5631910083ab835168fda1eb6f83c0)

--- source/class/cv/Application.js
+++ source/class/cv/Application.js
@@ -936,60 +936,63 @@ qx.Class.define('cv.Application',
           const env = req.getResponse();
           const serverVersionId = env.PHP_VERSION_ID;
           //const [major, minor] = env.phpversion.split('.').map(ver => parseInt(ver));
-          const parts = env.required_php_version.split(' ');
-          const disable = parts.some(constraint => {
-            const match = /^(>=|<|>|<=|\^)(\d+)\.(\d+)\.?(\d+)?$/.exec(constraint);
-            if (match) {
-              const operator = match[1];
-              const majorConstraint = parseInt(match[2]);
-              const hasMinorVersion = match[3] !== undefined;
-              const minorConstraint = hasMinorVersion ? parseInt(match[3]) : 0;
-              const hasPatchVersion = match[4] !== undefined;
-              const patchConstraint = hasPatchVersion ? parseInt(match[4]) : 0;
-              const constraintId = 10000 * majorConstraint + 100 * minorConstraint + patchConstraint;
-              const maxId = 10000 * majorConstraint + (hasMinorVersion ? 100 * minorConstraint : 999) + (hasPatchVersion ? patchConstraint : 99);
-              // incomplete implementation of: https://getcomposer.org/doc/articles/versions.md#writing-version-constraints
-              switch (operator) {
-                case '>=':
-                  if (serverVersionId < constraintId) {
-                    return true;
-                  }
-                  break;
-                case '>':
-                  if (serverVersionId <= constraintId) {
-                    return true;
-                  }
-                  break;
-                case '<=':
-                  if (serverVersionId > maxId) {
-                    return true;
-                  }
-                  break;
-                case '<':
-                  if (serverVersionId >= maxId) {
-                    return true;
-                  }
-                  break;
-                case '^':
-                  if (serverVersionId < constraintId || serverVersionId > 10000 *(majorConstraint+1)) {
-                    return true;
-                  }
-                  break;
-                case '~':
-                  if (serverVersionId < constraintId || hasPatchVersion ? serverVersionId > 10000 * (majorConstraint+1) : serverVersionId > (10000 *(majorConstraint) + 100 * (patchConstraint+1))) {
-                    return true;
-                  }
-                  break;
+          let disable = false;
+          if (Object.prototype.hasOwnProperty.call(env, 'required_php_version')) {
+            const parts = env.required_php_version.split(' ');
+            disable = parts.some(constraint => {
+              const match = /^(>=|<|>|<=|\^)(\d+)\.(\d+)\.?(\d+)?$/.exec(constraint);
+              if (match) {
+                const operator = match[1];
+                const majorConstraint = parseInt(match[2]);
+                const hasMinorVersion = match[3] !== undefined;
+                const minorConstraint = hasMinorVersion ? parseInt(match[3]) : 0;
+                const hasPatchVersion = match[4] !== undefined;
+                const patchConstraint = hasPatchVersion ? parseInt(match[4]) : 0;
+                const constraintId = 10000 * majorConstraint + 100 * minorConstraint + patchConstraint;
+                const maxId = 10000 * majorConstraint + (hasMinorVersion ? 100 * minorConstraint : 999) + (hasPatchVersion ? patchConstraint : 99);
+                // incomplete implementation of: https://getcomposer.org/doc/articles/versions.md#writing-version-constraints
+                switch (operator) {
+                  case '>=':
+                    if (serverVersionId < constraintId) {
+                      return true;
+                    }
+                    break;
+                  case '>':
+                    if (serverVersionId <= constraintId) {
+                      return true;
+                    }
+                    break;
+                  case '<=':
+                    if (serverVersionId > maxId) {
+                      return true;
+                    }
+                    break;
+                  case '<':
+                    if (serverVersionId >= maxId) {
+                      return true;
+                    }
+                    break;
+                  case '^':
+                    if (serverVersionId < constraintId || serverVersionId > 10000 * (majorConstraint + 1)) {
+                      return true;
+                    }
+                    break;
+                  case '~':
+                    if (serverVersionId < constraintId || hasPatchVersion ? serverVersionId > 10000 * (majorConstraint + 1) : serverVersionId > (10000 * (majorConstraint) + 100 * (patchConstraint + 1))) {
+                      return true;
+                    }
+                    break;
+                }
               }
+              return false;
+            });
+            if (disable) {
+              this.error('Disabling manager due to PHP version mismatch. Installed:', env.phpversion, 'required:', env.required_php_version);
+              this.setManagerDisabled(true);
+              this.setManagerDisabledReason(qx.locale.Manager.tr('Your system does not provide the required PHP version for the manager. Installed: %1, required: %2', env.phpversion, env.required_php_version));
+            } else {
+              this.info('Manager available for PHP version', env.phpversion);
             }
-            return false;
-          });
-          if (disable) {
-            this.error('Disabling manager due to PHP version mismatch. Installed:', env.phpversion, 'required:', env.required_php_version);
-            this.setManagerDisabled(true);
-            this.setManagerDisabledReason(qx.locale.Manager.tr('Your system does not provide the required PHP version for the manager. Installed: %1, required: %2', env.phpversion, env.required_php_version));
-          } else {
-            this.info('Manager available for PHP version', env.phpversion);
           }
           this.setManagerChecked(true);
         }, this);
