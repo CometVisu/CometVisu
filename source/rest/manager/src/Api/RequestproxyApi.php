8581f42635ae37aa1ee2f0779e05d98383bd7534
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-01-21 22:10:10 +0100

    revert change because its handled better by another changeset

--- source/rest/manager/src/Api/RequestproxyApi.php
+++ source/rest/manager/src/Api/RequestproxyApi.php
@@ -23,9 +23,6 @@ class RequestproxyApi extends AbstractRequestproxyApi {
     $selfSigned = Helper::getQueryParam($request, 'self-signed', 'false') === "true";
     $allowed = false;
     $configSection = [];
-    if (is_null($hidden)) {
-      $hidden = [];
-    }
     if ($hiddenConfigSection != null) {
       if (array_key_exists($hiddenConfigSection, $hidden)) {
         $configSection = $hidden[$hiddenConfigSection];
