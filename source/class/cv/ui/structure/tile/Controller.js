d962a9fa50fc476a2f11f90ea4c9f7e53c53fc8c
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-01-23 19:47:42 +0100

    add attribute for lazy loading

--- source/class/cv/ui/structure/tile/Controller.js
+++ source/class/cv/ui/structure/tile/Controller.js
@@ -322,7 +322,7 @@ qx.Class.define('cv.ui.structure.tile.Controller', {
       // find all pages with an iframe with attribute "data-src" and observe its parent page
       const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-          if (entry.isIntersecting && entry.target.nodeName.toLowerCase() === 'iframe' && entry.target.hasAttribute('data-src')) {
+          if (entry.isIntersecting && entry.target.hasAttribute('data-src')) {
             entry.target.setAttribute('src', entry.target.getAttribute('data-src'));
             entry.target.removeAttribute('data-src');
             observer.unobserve(entry.target);
@@ -331,7 +331,7 @@ qx.Class.define('cv.ui.structure.tile.Controller', {
           root: document.querySelector('body > main')
         });
       });
-      for (const iframe of document.querySelectorAll('iframe[data-src]')) {
+      for (const iframe of document.querySelectorAll('iframe[data-src], img[data-src]')) {
         observer.observe(iframe);
       }
     },
