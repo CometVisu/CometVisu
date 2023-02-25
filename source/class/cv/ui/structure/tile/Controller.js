a9834272a22e2dfb4efd6067f6eb797e5f534968
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-02-25 19:04:47 +0100

    add possibility to define attribute target name in a slot attribute, this fixes the wrong attribute mapping for the "value-format" attribute in the small-status widget
    also add the missing screenshots

--- source/class/cv/ui/structure/tile/Controller.js
+++ source/class/cv/ui/structure/tile/Controller.js
@@ -604,8 +604,23 @@ class TemplatedElement extends HTMLElement {
         } else if (name.endsWith('-format')) {
           targetName = 'format';
         }
-        targets.forEach(target => {
+        for (const target of targets) {
           if (targetName !== name && target.hasAttribute('slot-' + name)) {
+            const targetValue = target.getAttribute('slot-' + name);
+            if (targetValue.startsWith(':')) {
+              // this template slot-attribute contains some configuration
+              for (const entry of targetValue.substring(1).split(',')) {
+                const [key, value] = entry.split('=');
+                switch (key) {
+                  case 'target':
+                    name = value;
+                    break;
+                  default:
+                    qx.log.Logger.error(this, 'unhandled slot-attribute configuration key', key);
+                    break;
+                }
+              }
+            }
             target.setAttribute(name, value);
 
             target.removeAttribute('slot-' + name);
@@ -614,7 +629,7 @@ class TemplatedElement extends HTMLElement {
 
             target.removeAttribute('slot-' + targetName);
           }
-        });
+        }
         if (targets.length > 0) {
           this.removeAttribute(name);
         }
