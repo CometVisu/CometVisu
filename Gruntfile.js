8dc5176d5aafd0d2c688b411c68c05ad48031dbd
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-07-29 11:48:33 +0200

    remove banner config, intellij can do that

--- Gruntfile.js
+++ Gruntfile.js
@@ -212,51 +212,6 @@ module.exports = function(grunt) {
     branch = grunt.option('branch');
 
   var config = {
-
-    // license header adding
-    usebanner: {
-      dist: {
-        options: {
-          position: 'top',
-          replace: true,
-          linebreak: true,
-          process: function( filepath ) {
-            var filename = filepath.match(/\/([^/]*)$/)[1];
-            if (filename === "__init__.js") { return ""; }
-
-            return grunt.template.process('/* <%= filename %> \n'+
-              ' * \n'+
-              ' * copyright (c) 2010-<%= grunt.template.today("yyyy") %>, Christian Mayer and the CometVisu contributors.\n'+
-              ' * \n'+
-              ' * This program is free software; you can redistribute it and/or modify it\n'+
-              ' * under the terms of the GNU General Public License as published by the Free\n'+
-              ' * Software Foundation; either version 3 of the License, or (at your option)\n'+
-              ' * any later version.\n'+
-              ' *\n'+
-              ' * This program is distributed in the hope that it will be useful, but WITHOUT\n'+
-              ' * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or\n'+
-              ' * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for\n'+
-              ' * more details.\n'+
-              ' *\n'+
-              ' * You should have received a copy of the GNU General Public License along\n'+
-              ' * with this program; if not, write to the Free Software Foundation, Inc.,\n'+
-              ' * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA\n'+
-              ' */\n', {
-                data: {
-                  filename: filename,
-                  author: pkg.authors[0].name+ " ["+pkg.authors[0].email+"]",
-                  version: pkg.version
-                }
-              }
-            );
-          }
-        },
-        files: {
-          src: sourceFiles.concat(['source/test/**/*.js'])
-        }
-      }
-    },
-
     // make a zipfile
     compress: {
       qxClient: {
