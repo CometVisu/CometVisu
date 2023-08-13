dd14618e963cebbd955e9ef93e4e20439c200ece
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-08-13 11:34:33 +0200

    bring back the source editor for special text node content editing. Also prepare to handle "flux" correctly. It is not supported by monaco editor by now. but with this preparation it will work when support is added without a change on out side.

--- source/class/cv/ui/manager/editor/Tree.js
+++ source/class/cv/ui/manager/editor/Tree.js
@@ -1876,24 +1876,42 @@ qx.Class.define('cv.ui.manager.editor.Tree', {
           }
         };
 
-        if (element.isTextNode() && element.getParent().getName() === 'status') {
-          const type = element.getParent().getAttribute('type');
-          if ((type === 'html' || type === 'xml') && element.getNode().nodeType === Node.TEXT_NODE) {
+        if (element.isTextNode()) {
+          let type = '';
+          const parent = element.getParent();
+          let convertToCData = false;
+          switch (parent.getName()) {
+            case 'status':
+              type = element.getParent().getAttribute('type');
+              convertToCData = (type === 'html' || type === 'xml');
+              break;
+
+            case 'style':
+              type = 'css';
+              convertToCData = true;
+              break;
+
+            case 'dataset':
+              if (parent.getAttribute('src').startsWith("flux://")) {
+                type = 'flux';
+              }
+              break;
+          }
+          if (convertToCData && element.getNode().nodeType === Node.TEXT_NODE) {
             element.convertTextNodeType(Node.CDATA_SECTION_NODE);
             const newNodeName = element.getNode().nodeName;
             formData[newNodeName] = formData[nodeName];
             delete formData[nodeName];
             nodeName = newNodeName;
           }
-          // Due to a bug that swallowed whitespaces in the monaco editor from time to time this is disabled for now
 
           // special handling for status content: check of source editor supports the type and use it instead of a plain TextArea
-          /*if (type && cv.ui.manager.editor.Source.SUPPORTED_FILES("test." + type)) {
+          if (type && cv.ui.manager.editor.Source.SUPPORTED_FILES("test." + type)) {
             formData[nodeName].type = "SourceEditor";
             formData[nodeName].language = type;
             formData[nodeName].width = Math.min(qx.bom.Viewport.getWidth(), 800);
             delete formData[nodeName].placeholder;
-          }*/
+          }
         }
         this.__checkProvider(
           element.getParent().getName() + '@' + element.getName(),
