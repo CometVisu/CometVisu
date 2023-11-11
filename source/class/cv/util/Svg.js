13ad65307960dbdc132e8660a34473a22e4c150f
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-11-11 13:00:04 +0100

    use self if it is a svg and has no owner

--- source/class/cv/util/Svg.js
+++ source/class/cv/util/Svg.js
@@ -29,7 +29,7 @@ qx.Class.define('cv.util.Svg', {
   statics: {
     getBBox(element) {
       const bbox = { x: 0, y: 0, cx: 0, cy: 0, width: 0, height: 0 };
-      const svg = element.ownerSVGElement;
+      const svg = element.ownerSVGElement || element.localName === "svg" ? element : null;
       if (svg) {
         if (qx.core.Environment.get('browser.name').includes('firefox')) {
           // firefox getScreenCTM does not work well, we have to do it manually
