00804324300f06b432b76f9ff3f942e585b822b7
Tobias Bräutigam <tbraeutigam@gmail.com>
2023-12-27 12:36:07 +0100

    use selenium-manager from npm package

--- utils/ci/browser-setup.sh
+++ utils/ci/browser-setup.sh
@@ -8,27 +8,26 @@ if [ "$CV_BROWSER" = "" ]; then
     exit 0
 fi
 
-curl --silent --show-error --location --remote-name https://github.com/SeleniumHQ/selenium/raw/trunk/common/manager/linux/selenium-manager
-chmod +x selenium-manager
-./selenium-manager --version
+SELENIUM_MANAGER=./node_modules/selenium-webdriver/bin/linux/selenium-manager
+$SELENIUM_MANAGER --version
 
 if [ $CV_BROWSER = Firefox ]; then
-    OUTPUT=$(./selenium-manager --browser=firefox --browser-version=$CV_VERSION)
+    OUTPUT=$($SELENIUM_MANAGER --browser=firefox --browser-version=$CV_VERSION)
     DRIVER_PATH=$(printf ${OUTPUT} | grep "Driver path" | awk '{print $4}')
     BROWSER_PATH=$(printf ${OUTPUT} | grep "Browser path" | awk '{print $4}')
     ln -s -f ${BROWSER_PATH} firefox
     echo WEBDRIVER_PATH=${DRIVER_PATH} | tee .protractor-env
     echo BROWSER_PATH=${BROWSER_PATH} | tee -a .protractor-env
 else
-  OUTPUT=$(./selenium-manager --browser=chrome --browser-version=$CV_VERSION)
+  OUTPUT=$($SELENIUM_MANAGER --browser=chrome --browser-version=$CV_VERSION)
   BROWSER_PATH=$(printf "${OUTPUT}" | grep "Browser path" | awk '{print $4}')
   CHROME_FULL_VERSION=$($BROWSER_PATH --version | awk '{print $3}')
-  OUTPUT=$(./selenium-manager --browser=chrome --browser-version=$CV_VERSION --driver chromedriver --driver-version=$CHROME_FULL_VERSION)
+  OUTPUT=$($SELENIUM_MANAGER --browser=chrome --browser-version=$CV_VERSION --driver chromedriver --driver-version=$CHROME_FULL_VERSION)
   DRIVER_PATH=$(printf "${OUTPUT}" | grep "Driver path" | awk '{print $4}')
   ln -s -f ${BROWSER_PATH} chrome
   echo WEBDRIVER_PATH=${DRIVER_PATH} | tee .protractor-env
   echo BROWSER_PATH=${BROWSER_PATH} | tee -a .protractor-env
   mkdir -p ./node_modules/protractor/node_modules/webdriver-manager/selenium/
   cp ${DRIVER_PATH} ./node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_${CHROME_FULL_VERSION}
-  ./selenium-manager --browser=chrome --browser-version=$CV_VERSION --debug
+  $SELENIUM_MANAGER --browser=chrome --browser-version=$CV_VERSION --debug
 fi
