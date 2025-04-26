fdfde3eca7c5ca2824799afb10769ffeb5c64297
Tobias Bräutigam <tbraeutigam@gmail.com>
2025-04-26 08:09:37 +0200

    tests: add more chart tests

--- source/test/karma/ui/structure/tile/components/Chart-spec.js
+++ source/test/karma/ui/structure/tile/components/Chart-spec.js
@@ -44,4 +44,43 @@ describe('testing the <cv-chart> component of the tile structure', () => {
     expect(element.tagName).toBe('CV-CHART');
     expect(element._instance instanceof cv.ui.structure.tile.components.Chart).toBe(true);
   });
+
+  it('should create a chart with title', function(done) {
+    const element = this.createTileWidgetWithComponent('cv-chart', {title: 'Test'}, '');
+    const widget = element.parentElement.parentElement;
+    // charts has a sync init function
+    setTimeout(() => {
+      expect(widget.querySelector(':scope > header > label.title')).not.toBeNull();
+      done();
+    }, 100);
+  });
+
+  const matrix = [
+    {selection: 'none', series: 'day', expected: []},
+    {selection: 'all', series: 'day', expected: ['hour', 'day', 'week', 'month', 'year']},
+    {selection: 'hour,day', series: 'week', expected: ['hour', 'day', 'week']}
+  ];
+
+  for (const {selection, series, expected} of matrix) {
+    it(`should create a chart with ${selection} selection and ${series} series`, function(done) {
+      const element = this.createTileWidgetWithComponent('cv-chart', { selection, series }, '');
+      const widget = element.parentElement.parentElement;
+
+      setTimeout(() => {
+        expect(element._instance.getCurrentSeries()).toBe(series);
+        expect(widget.querySelectorAll(':scope > header > label.title > .popup.series > cv-option').length).toBe(expected.length);
+        if (expected.length > 0) {
+          expect(widget.querySelector(':scope > header > button.prev')).not.toBeNull();
+          expect(widget.querySelector(':scope > header > button.next')).not.toBeNull();
+        } else {
+          expect(widget.querySelector(':scope > header > button.prev')).toBeNull();
+          expect(widget.querySelector(':scope > header > button.next')).toBeNull();
+        }
+        for (const option of expected) {
+          expect(widget.querySelector(`:scope > header > label.title > .popup.series > cv-option[key="${option}"]`)).not.toBeNull();
+        }
+        done();
+      }, 100);
+    });
+  }
 });
