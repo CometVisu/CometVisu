cf225feafd499245450643c080860c44b453b1ad
Tobias Bräutigam <tbraeutigam@gmail.com>
2025-08-17 16:04:19 +0200

    test: make eslint ignore it

--- source/class/cv/ui/structure/tile/widgets/Tile.js
+++ source/class/cv/ui/structure/tile/widgets/Tile.js
@@ -186,6 +186,7 @@ qx.Class.define('cv.ui.structure.tile.widgets.Tile', {
     },
 
     checkOutdated() {
+      // eslint-disable-next-line no-console
       console.log('###', this._lastUpdate);
       if (this._lastUpdate instanceof Date) {
         if (isNaN(this._lastUpdate.getTime())) {
